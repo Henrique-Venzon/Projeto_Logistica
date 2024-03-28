693baa67601fc4e352b1fd975d11fc3aeb2b1e7e
Henrique Francisco Venzon <henriquevenzon.f@gmail.com>
2024-03-28 11:54:24 -0300

    regv

--- login.php
+++ login.php
@@ -11,27 +11,21 @@
     <div class="container">
         <div class="content first-content">
             <div class="first-column">
-                <h2 class="title title-primary">Entrar</h2>
-                <p class="description description-primary">Já tem uma conta?</p>
-                <button id="signin" class="btn btn-primary"><h3>Entrar</h3></button>
+                <h2 class="title title-primary">Seja<br>Bem-Vindo!</h2>
             </div>    
             <div class="second-column">
                 <h2 class="title title-second">Crie sua conta</h2>
                 <p class="description description-second">Use seu email para o registro:</p>
                 <form class="form">
                     <label class="label-input" for="">
-                        <input type="text" placeholder="Nome">
+                        <input type="text" placeholder="Usuario">
                     </label>
-                    
-                    <label class="label-input" for="">
-                        <input type="email" placeholder="Email">
-                    </label>
-                    
+         
                     <label class="label-input" for="">
                         <input type="password" placeholder="Senha">
                     </label>
                     
-                    <button class="btn btn-second"><h3 class="ajeitarcadastro">Cadastrar</h3></button>        
+                    <button class="btn btn-second"><h3 class="ajeitarcadastro">Entrar</h3></button>        
                 </form>
             </div>
         
