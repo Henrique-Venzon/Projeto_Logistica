a881f642a7ff48ec9f098bbfc2e4e54e6d42a7be
Noobpro112 <reismatheusyan83@gmail.com>
2024-03-29 16:28:26 -0300

    Limite de armazem criado

---
+++ getDados.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root.Att";
+$password = "root";
+$dbname = "logistica";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT quantidade_atual, limite_maximo FROM armazem WHERE id=1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo json_encode($row);
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
