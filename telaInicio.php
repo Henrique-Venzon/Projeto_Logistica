5579b041398968c07a33b792e0d8980e17d77840
Henrique Francisco Venzon <henriquevenzon.f@gmail.com>
2024-04-02 08:56:55 -0300

    t

--- telaInicio.php
+++ telaInicio.php
@@ -19,31 +19,42 @@
             </div>
         </header>
 
-        <aside>
-            <input type="checkbox" id="chec">
+        <nav>
+            <ul class="ulPrincipal">
+            <li><input type="checkbox" id="chec">
             <div class="coisas">
                 <label for="chec">
                 <img src="" alt="">
                 </label>
+                <div class="aparecer">
                 <h1>Docas</h1>
+                </div>
             </div>
             <div class="coisas">
             <label for="chec">
                 <img src="" alt="">
                 </label>
+                <div class="aparecer">
                 <h1>Pikin</h1>
+                </div>
             </div>
             <div class="coisas">
             <label for="chec">
                 <img src="" alt="">
+                <div class="aparecer">
+                </div>
                 <h1>Pedidos</h1>
             </div>
             <div class="coisas">
             <label for="chec">
                 <img src="" alt="">
+                <div class="aparecer">
                 <h1>Configuração</h1>
             </div>
-        </aside>
+            </div>
+            </li>
+            </ul>
+</nav>
         
         <script src="" async defer></script>
     </body>
