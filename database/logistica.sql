c5f03e20aac1751f140d780e42600986228b2789
Henrique Francisco Venzon <henriquevenzon.f@gmail.com>
2024-04-02 08:59:29 -0300

    Update logistica.sql

--- database/logistica.sql
+++ database/logistica.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 02/04/2024 às 11:29
--- Versão do servidor: 8.0.36
--- Versão do PHP: 8.2.13
+-- Host: 127.0.0.1
+-- Tempo de geração: 02-Abr-2024 às 13:59
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -15,7 +15,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES latin1 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `logistica`
@@ -26,81 +26,74 @@ USE `logistica`;
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `armazem`
+-- Estrutura da tabela `armazem`
 --
 
 DROP TABLE IF EXISTS `armazem`;
-CREATE TABLE IF NOT EXISTS `armazem` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `quantidade_atual` int NOT NULL,
-  `limite_maximo` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `armazem` (
+  `id` int(11) NOT NULL,
+  `quantidade_atual` int(11) NOT NULL,
+  `limite_maximo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Despejando dados para a tabela `armazem`
+-- Extraindo dados da tabela `armazem`
 --
 
 INSERT INTO `armazem` (`id`, `quantidade_atual`, `limite_maximo`) VALUES
-(1, 5, 10);
+(1, 6, 10);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `itens_notas_fiscais`
+-- Estrutura da tabela `itens_notas_fiscais`
 --
 
 DROP TABLE IF EXISTS `itens_notas_fiscais`;
-CREATE TABLE IF NOT EXISTS `itens_notas_fiscais` (
-  `NUMERO` int NOT NULL,
+CREATE TABLE `itens_notas_fiscais` (
+  `NUMERO` int(11) NOT NULL,
   `CODIGO_DO_PRODUTO` varchar(10) NOT NULL,
-  `QUANTIDADE` int NOT NULL,
-  `PRECO` float NOT NULL,
-  PRIMARY KEY (`NUMERO`,`CODIGO_DO_PRODUTO`),
-  KEY `CODIGO_DO_PRODUTO` (`CODIGO_DO_PRODUTO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `QUANTIDADE` int(11) NOT NULL,
+  `PRECO` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `logins`
+-- Estrutura da tabela `logins`
 --
 
 DROP TABLE IF EXISTS `logins`;
-CREATE TABLE IF NOT EXISTS `logins` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `logins` (
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `tipo_login` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `tipo_login` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `notas_fiscais`
+-- Estrutura da tabela `notas_fiscais`
 --
 
 DROP TABLE IF EXISTS `notas_fiscais`;
-CREATE TABLE IF NOT EXISTS `notas_fiscais` (
+CREATE TABLE `notas_fiscais` (
   `CPF` varchar(11) NOT NULL,
   `MATRICULA` varchar(5) NOT NULL,
   `DATA_VENDA` date DEFAULT NULL,
-  `NUMERO` int NOT NULL,
-  `IMPOSTO` float NOT NULL,
-  PRIMARY KEY (`NUMERO`),
-  KEY `MATRICULA` (`MATRICULA`),
-  KEY `CPF` (`CPF`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `NUMERO` int(11) NOT NULL,
+  `IMPOSTO` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tabela_de_clientes`
+-- Estrutura da tabela `tabela_de_clientes`
 --
 
 DROP TABLE IF EXISTS `tabela_de_clientes`;
-CREATE TABLE IF NOT EXISTS `tabela_de_clientes` (
+CREATE TABLE `tabela_de_clientes` (
   `CPF` varchar(11) NOT NULL,
   `NOME` varchar(100) DEFAULT NULL,
   `ENDERECO_1` varchar(150) DEFAULT NULL,
@@ -110,61 +103,123 @@ CREATE TABLE IF NOT EXISTS `tabela_de_clientes` (
   `ESTADO` varchar(2) DEFAULT NULL,
   `CEP` varchar(8) DEFAULT NULL,
   `DATA_DE_NASCIMENTO` date DEFAULT NULL,
-  `IDADE` smallint DEFAULT NULL,
+  `IDADE` smallint(6) DEFAULT NULL,
   `SEXO` varchar(1) DEFAULT NULL,
   `LIMITE_DE_CREDITO` float DEFAULT NULL,
   `VOLUME_DE_COMPRA` float DEFAULT NULL,
-  `PRIMEIRA_COMPRA` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`CPF`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `PRIMEIRA_COMPRA` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tabela_de_produtos`
+-- Estrutura da tabela `tabela_de_produtos`
 --
 
 DROP TABLE IF EXISTS `tabela_de_produtos`;
-CREATE TABLE IF NOT EXISTS `tabela_de_produtos` (
+CREATE TABLE `tabela_de_produtos` (
   `CODIGO_DO_PRODUTO` varchar(10) NOT NULL,
   `NOME_DO_PRODUTO` varchar(50) DEFAULT NULL,
   `EMBALAGEM` varchar(20) DEFAULT NULL,
   `TAMANHO` varchar(10) DEFAULT NULL,
   `SABOR` varchar(20) DEFAULT NULL,
-  `PRECO_DE_LISTA` float NOT NULL,
-  PRIMARY KEY (`CODIGO_DO_PRODUTO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `PRECO_DE_LISTA` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `tabela_de_vendedores`
+-- Estrutura da tabela `tabela_de_vendedores`
 --
 
 DROP TABLE IF EXISTS `tabela_de_vendedores`;
-CREATE TABLE IF NOT EXISTS `tabela_de_vendedores` (
+CREATE TABLE `tabela_de_vendedores` (
   `MATRICULA` varchar(5) NOT NULL,
   `NOME` varchar(100) DEFAULT NULL,
   `PERCENTUAL_COMISSAO` float DEFAULT NULL,
   `DATA_ADMISSAO` date DEFAULT NULL,
   `DE_FERIAS` bit(1) DEFAULT NULL,
-  `BAIRRO` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`MATRICULA`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `BAIRRO` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Restrições para tabelas despejadas
+-- Índices para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `itens_notas_fiscais`
+-- Índices para tabela `armazem`
+--
+ALTER TABLE `armazem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `itens_notas_fiscais`
+--
+ALTER TABLE `itens_notas_fiscais`
+  ADD PRIMARY KEY (`NUMERO`,`CODIGO_DO_PRODUTO`),
+  ADD KEY `CODIGO_DO_PRODUTO` (`CODIGO_DO_PRODUTO`);
+
+--
+-- Índices para tabela `logins`
+--
+ALTER TABLE `logins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `notas_fiscais`
+--
+ALTER TABLE `notas_fiscais`
+  ADD PRIMARY KEY (`NUMERO`),
+  ADD KEY `MATRICULA` (`MATRICULA`),
+  ADD KEY `CPF` (`CPF`);
+
+--
+-- Índices para tabela `tabela_de_clientes`
+--
+ALTER TABLE `tabela_de_clientes`
+  ADD PRIMARY KEY (`CPF`);
+
+--
+-- Índices para tabela `tabela_de_produtos`
+--
+ALTER TABLE `tabela_de_produtos`
+  ADD PRIMARY KEY (`CODIGO_DO_PRODUTO`);
+
+--
+-- Índices para tabela `tabela_de_vendedores`
+--
+ALTER TABLE `tabela_de_vendedores`
+  ADD PRIMARY KEY (`MATRICULA`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `armazem`
+--
+ALTER TABLE `armazem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `logins`
+--
+ALTER TABLE `logins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `itens_notas_fiscais`
 --
 ALTER TABLE `itens_notas_fiscais`
   ADD CONSTRAINT `itens_notas_fiscais_ibfk_1` FOREIGN KEY (`CODIGO_DO_PRODUTO`) REFERENCES `tabela_de_produtos` (`CODIGO_DO_PRODUTO`),
   ADD CONSTRAINT `itens_notas_fiscais_ibfk_2` FOREIGN KEY (`NUMERO`) REFERENCES `notas_fiscais` (`NUMERO`);
 
 --
--- Restrições para tabelas `notas_fiscais`
+-- Limitadores para a tabela `notas_fiscais`
 --
 ALTER TABLE `notas_fiscais`
   ADD CONSTRAINT `notas_fiscais_ibfk_1` FOREIGN KEY (`MATRICULA`) REFERENCES `tabela_de_vendedores` (`MATRICULA`),
