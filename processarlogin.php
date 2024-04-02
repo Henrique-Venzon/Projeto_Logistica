acb0f5f69856593fdc90ddfa76971fdf2b96d913
Matheus Yan <reismatheusyan83@gmail.com>
2024-04-02 11:53:20 -0300

    Login Funcional + limite padrão

--- processarlogin.php
+++ processarlogin.php
@@ -1,39 +1,47 @@
-<html>
+<?php
+// iniciar uma sessão
+session_start();
 
-<body>
+$hostname = "127.0.0.1";
+$user = "root.Att";
+$password = "root";
+$database = "logistica";
 
-    <?php
-    // iniciar uma sessão
-    session_start();
+$conexao = new mysqli($hostname, $user, $password, $database);
 
-    $hostname = "127.0.0.1";
-    $user = "";
-    $password = "";
-    $database = "logistica";
-
-    $conexao = new mysqli($hostname, $user, $password, $database);
-
-    if ($conexao->connect_errno) {
-        echo "Failed to connect to MySQL: " . $conexao->connect_error;
-        exit();
-    } else {
+if ($conexao->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conexao->connect_error;
+    exit();
+} else {
+    if (isset($_POST['Usuario']) && isset($_POST['Senha'])) {
         // Evita caracteres epsciais (SQL Inject)
-        $nomeUsuario = $conexao->real_escape_string($_POST['nomeUsuario']);
-        $senha = $conexao->real_escape_string($_POST['senha']);
+        $username = $conexao->real_escape_string($_POST['Usuario']);
+        $password = $conexao->real_escape_string($_POST['Senha']);
 
-        $sql = "SELECT `idUsuario`, `nomeUsuario` FROM `usuario`
-					WHERE `nomeUsuario` = '" . $nomeUsuario . "'
-					AND `senha` = '" . $senha . ";";
+        $sql = "SELECT `id`, `username` FROM `logins`
+                    WHERE `username` = '" . $username . "'
+                    AND `password` = '" . $password . "';";
 
         $resultado = $conexao->query($sql);
 
         if ($resultado->num_rows != 0) {
             $row = $resultado->fetch_array();
-            $_SESSION['idUsuario'] = $row[0];
-            $_SESSION['nomeUsuario'] = $row[1];
+            $_SESSION['id'] = $row[0];
+            $_SESSION['username'] = $row[1];
+
+            // Verificar se já existe um registro para esse usuário na tabela armazem
+            $sql = "SELECT `id` FROM `armazem_limite` WHERE `login_id` = " . $_SESSION['id'] . ";";
+            $resultado = $conexao->query($sql);
+
+            if ($resultado->num_rows == 0) {
+                // Se não existir, inserir um novo registro com o limite máximo padrão de 300
+                $sql = "INSERT INTO `armazem_limite` (`login_id`, `quantidade_atual`, `limite_maximo`) VALUES (" . $_SESSION['id'] . ", 0, 300);";
+                $conexao->query($sql);
+            }
+
             $conexao->close();
 
-            header('Location: telaIncio.php', true, 301);
+            header('Location: telaInicio.php', true, 301);
             exit();
         } else {
 
@@ -41,7 +49,5 @@
             header('Location: index.php', true, 301);
         }
     }
-    ?>
-</body>
-
-</html>
+}
+?>
