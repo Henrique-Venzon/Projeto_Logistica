459a8bfed3a3e86a974481d15429104c837b5df3
Henrique Francisco Venzon <henriquevenzon.f@gmail.com>
2024-04-02 07:58:53 -0300

    j8

--- telaInicio.php
+++ telaInicio.php
@@ -8,9 +8,15 @@
     </head>
     <body>
         <header>
+            <div class="imagemDec">
             <img src="" alt="">
+            </div>
+            <div class="textArmazem">
             <h1>Armazém</h1>
+            </div>
+            <div class="imgperfil">
             <img src="" alt="">
+            </div>
         </header>
         
         <script src="" async defer></script>
