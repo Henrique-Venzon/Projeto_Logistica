74b073c2004e775b6550299caf3470cbc5de3759
Henrique Francisco Venzon <henriquevenzon.f@gmail.com>
2024-04-11 08:57:03 -0300

    g

--- criarpedido.php
+++ criarpedido.php
@@ -1,11 +1,12 @@
 <div>
     <div class="">
         <form class="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <label for="quantidade">Quantidade de produtos do pedido:</label>
+            <label for="quantidade">Id do Pedido:</label>
             <input type="text" id="id_pedido" name="id_pedido">
+            <br>
 
             <label for="quantidade">Quantidade de produtos do pedido:</label>
-            <input type="number" id="quantidade" name="quantidade" min="1" max="100" onchange="gerarCampos()">
+            <input type="number" id="quantidade" name="quantidade_de_produtos" min="1" max="100" onchange="gerarCampos()">
 
             <div id="camposProdutos">
                 <!-- local dos campos do produto:  -->
@@ -29,10 +30,29 @@ function gerarCampos() {
             </label>
             <br>
             
-            <label class="label-input" for="">OCM: <br>
-                <input type="text" name="nome_produto[]" placeholder="Nome do produto" id="nome_produto${i}">
+            <label class="label-input" for="">UN: <br>
+            <select name="unidade_produto[]" id="unidade_produto${i}">
+            <option value="UN">UN - Unidade</option>
+            <option value="KG">KG - Quilograma</option>
+            <option value="L">L - Litro</option>
+            <option value="M">M - Metro</option>
+            <option value="CM">CM - Centímetro</option>
+            <option value="MM">MM - Milímetro</option>
+            <option value="M2">M² - Metro quadrado</option>
+            <option value="M3">M³ - Metro cúbico</option>
+            <option value="CX">CX - Caixa</option>
+            <option value="PC">PC - Peça</option>
+            <option value="RL">RL - Rolo</option>
+            <option value="FD">FD - Fardo</option>
+            </select>
             </label>
             <br>
+            <label for="quantidade">Quantidade: </label>
+            <input type="number" id="quantidade" name="quantidade" min="1">
+            <br>
+            <label for="quantidade">Quantidade: </label>
+            <input type="text" id="quantidade" name="quantidade" pattern="\\d{8}" title="Insira exatamente 8 dígitos." required>
+
             
             <label class="label-input" for="">CFOP: <br>
                 <input type="text" name="nome_produto[]" placeholder="Nome do produto" id="nome_produto${i}">
