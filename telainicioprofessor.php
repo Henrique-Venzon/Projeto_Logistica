6550deb2842cfebc0e4c8875cbcf45696dafaa2f
Henrique Francisco Venzon <henriquevenzon.f@gmail.com>
2024-04-16 07:35:45 -0300

    e

--- telainicioprofessor.php
+++ telainicioprofessor.php
@@ -31,44 +31,80 @@
         </div>
     </header>
     <main>
-        <div class="DivEsquerda">
-            <ul class="Ulprincipal">
-                <li><input type="checkbox" id="chec">
-                    <label for="chec">
-                        <div class="label1">
-                            <div class="img-T"><img src="img/caminhao.png" style="cursor: pointer;" class="imgbotao">
-                            </div>
-                            <div class="img-T"><img src="img/Piking.png" style="cursor: pointer;" class="imgbotao">
-                            </div>
-                            <div class="img-T"><img src="img/pedidos.png" style="cursor: pointer;" class="imgbotao">
-                            </div>
-                            <div class="img-T"><img src="img/armazempreto.png" style="cursor: pointer;" class="imgbotao">
-                            </div>
-                            <div class="img-T"><img src="img/confpreto.png" style="cursor: pointer;" class="imgbotao">
-                            </div>
-                        </div>
-                    </label>
-                    <nav>
-                        <ul class="Ulvertical">
-                            <div class="text">
-                                <h1>Docas </h1>
-                            </div>
-                            <div class="text">
-                                <h1>Piking</h1>
-                            </div>
-                            <div class="text">
-                                <h1>Pedidos</h1>
-                            </div>
-                            <div class="text">
-                                <h1>Armazém</h1>
-                            </div>
-                            <div class="text">
-                                <h1>Controle</h1>
-                            </div>
+    <div class="menuL">
+            <aside id="sidebar">
+                <div class="d-flex">
+                    <button class="toggle-btn" type="button">
+                        <img class="menuImg" src="img/menu.png" alt="">
+                    </button>
+                    <div class="sidebar-logo">
+            <a href="#" >Nome</a>
+                    </div>
+                </div>
+                <ul class="sidebar-nav">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="img/perfil.png" alt="">
+                            <span>Perfil</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="" alt="">
+                            <span>Movimentações</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="img/Piking.png" alt=""><i class="lni lni-agenda"></i>
+                            <span>Piking</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="" alt=""><i class="lni lni-agenda"></i>
+                            <span>Estoque</span>
+                        </a>
+                    </li>
+                    
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                            data-bs-target="#Recebimentos" aria-expanded="false" aria-controls="Recebimentos">
+                            <img src="img/caminhao.png" alt="">
+                            <span>Recebimentos</span>
+                        </a>
+                        <ul id="Recebimentos" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Container</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Carga</a>
+                            </li>
                         </ul>
-                    </nav>
-                </li>
-            </ul>
+                    </li> 
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="" alt=""><i class="lni lni-agenda"></i>
+                            <span>Expedição</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="" alt=""><i class="lni lni-agenda"></i>
+                            <span>Relatórios</span>
+                        </a>
+                    </li>
+                    
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <img src="img/confpreto.png" alt="">
+                            <span>Controle</span>
+                        </a>
+                    </li>
+                </ul>
+                
+            </aside>
+            
         </div>
         <div class="DivDireita">
             <div class="inicioConteudo">
