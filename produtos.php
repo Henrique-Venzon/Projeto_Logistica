5b445be39dc81039f0532935728f8fd417717865
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2024-04-24 12:33:06 -0300

    save

--- produtos.php
+++ produtos.php
@@ -8,89 +8,101 @@
 </head>
 
 <body>
+<?php
+    include 'include/header.php'
+    ?>
+    <main>
+        <?php
+        include 'include/menuLateral.php'
+        ?>
+  <div class="DivDireita">
+            <div class="table-inputs">
+              
+                <div class="form">
+         
+                <div class="ordem-form">
 
-<div class="DivDireita">
-<div class="table-inputs">
+<form action="" method="post">
+  <label for="ordem-numero">Pedido Nº</label>
+  <input type="text" id="" name="">
 
 
 
-<div class="ordem-form">
+</div>
+<label for="produto">NCM</label>
+<div class="input">
+    
 
-  <form action="" method="post">
-    <label for="ordem-numero">Pedido Nº</label>
-    <input type="text" id="" name="">
-
-
+  <input type="text" id="" name="">
   
-</div>
-<label for="produto">Produtos</label>
-<div class="input">
-
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
 </div>
 
 <div class="input">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
 </div>
 
 <div class="input">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
+  
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
 </div>
 
 <div class="input">
-    <input type="text" id="" name="" repla>
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
+
+  <input type="text" id="" name="" repla>
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
 </div>
 
 <div class="input">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
 </div>
 
 <div class="input">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
-    <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
+  <input type="text" id="" name="">
 </div>
 
 
 
 <div class="descricao">
-    <label for="descricao">Descrição</label>
-    <textarea id="descricao" name="descricao" oninput="this.style.height = ''; this.style.height = this.scrollHeight + 'px'"></textarea>
+  <label for="descricao">Descrição</label>
+  <textarea id="descricao" name="descricao" oninput="this.style.height = ''; this.style.height = this.scrollHeight + 'px'"></textarea>
 </div>
 
 
 <div class="buttons">
-    <button type="submit" class="button">Enviar</button>
-    <button type="button" class="button">Adicionar</button>
+  <button type="submit" class="button">Enviar</button>
+  <button type="button" class="button">Adicionar</button>
 </div>
+               
+                    </div>
+                </form>
+                    
+            </div>
+        </div>
+</main>
 
-    </form>
-    
-</div>
-
-</div>
 
 </body>
 </html>
