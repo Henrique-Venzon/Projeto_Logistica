3bb82da0b1eda303e6c67f6ddb8ed075a171af45
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-04-30 10:34:36 -0300

    u

--- include/menuLateral.php
+++ include/menuLateral.php
@@ -1,7 +1,6 @@
 
 <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
 
 <style>
 
@@ -272,6 +271,4 @@ a.sidebar-link:hover {
                 
             </aside>
             </div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+
