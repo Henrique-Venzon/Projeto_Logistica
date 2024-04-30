5303b2441dced5c6caed2a797a5cfd007cada753
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-04-30 10:32:58 -0300

    t

--- container.php
+++ container.php
@@ -181,10 +181,7 @@ if(!isset($_SESSION['id'])){
             </div>
         </div>
                 
-
     </main>
-
-    
 
     <script src="js/sidebar.js"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
