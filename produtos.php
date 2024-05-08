37f913ee96cc21a4a2b70babaf470729151b1103
lorez <lolitomg07@gmail.com>
2024-05-08 08:19:11 -0300

    x

--- produtos.php
+++ produtos.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 session_start();
 if(!isset($_SESSION['id'])){
@@ -132,3 +133,106 @@ if(!isset($_SESSION['id'])){
         crossorigin="anonymous"></script>
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Formulário de Pedido</title>
+<link rel="stylesheet" href="style_produto.css">
+</head>
+
+<body>
+
+<?php
+include'header.php'
+?>
+<main>
+    <?php 
+    include'menuLateal.php'
+    ?>
+
+    <div class="DivDireita">
+</div>
+
+<div class="ordem-form">
+  <form action="" method="post">
+    <label for="ordem-numero">Pedido Nº</label>
+    <input type="text" id="" name="">
+
+    <div class="input">
+    <label for="produto">Produtos</label>
+   
+
+  
+</div>
+
+<div class="input">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+</div>
+
+<div class="input">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+</div>
+
+<div class="input">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+</div>
+
+<div class="input">
+    <input type="text" id="" name="" repla>
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+</div>
+
+<div class="input">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+</div>
+
+<div class="input">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+    <input type="text" id="" name="">
+</div>
+
+
+
+<div class="descricao">
+    <label for="descricao">Descrição</label>
+    <textarea id="descricao" name="descricao" oninput="this.style.height = ''; this.style.height = this.scrollHeight + 'px' "></textarea>
+</div>
+
+
+<div class="buttons">
+    <button type="submit" class="button">Enviar</button>
+    <button type="button" class="button">Adicionar</button>
+</div>
+  </form>
+</div>
+</div>
+</main>
+
+</body>
+</html>
+>>>>>>> Stashed changes
