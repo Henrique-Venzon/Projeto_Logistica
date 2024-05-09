177acb78251dcdbfb4ac874027745a0d1d8804c6
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2024-05-09 09:35:44 -0300

    save

--- containerP.php
+++ containerP.php
@@ -4,9 +4,9 @@ if (!isset($_SESSION['id'])) {
     header("Location: index.php");
     exit;
 }
-if(!isset($_SESSION['turma'])){
-  header("Location: index.php");
-  exit;
+if (!isset($_SESSION['turma'])) {
+    header("Location: index.php");
+    exit;
 }
 ?>
 <!DOCTYPE html>
@@ -45,188 +45,188 @@ if(!isset($_SESSION['turma'])){
             <div class="table-inputs">
                 <div class="txtCont">
                     <h1>Criar pedido</h1>
-                    </div>
-                    <div class="form">
-        <div class="tabela-scroll">
-        <form action="processamento/processarcontainer.php" method="post">
-                    <div class="colunas1">
-                    <div class="numeroPedido">
-        <label for="num">N° pedido:</label>
-        <input id="num" type="number"Nome="npedido"  >
-        </div>
-        <div class="NomeDaEmpresa">
-        <label for="empresa">Empresa:</label>
-        <input id="empresa" type="text"Nome="Empresa" >
-        </div>
-          <div class="cliente">
-        <label for="nomeCliente">Cliente:</label>
-        <input id="nomeCliente" type="text"Nome="Cliente" >
-        </div>
-        <div class="telefone">
-        <label for="telefone">Telefone:</label>
-        <input id="telefone" type="number"Nome="Telefone" >
-        </div>
-        <div class="cep">
-        <label for="cep">CEP:</label>
-        <input id="cep" type="number" Nome="CEP">
-        </div>
-    </div>
-
-    <div class="colunas">
-    <div class="produto">
-      <label for="produto">Nome do Produto:</label>
-      <input type="text" id="produto" name="produto1" >
-      <input type="text" id="produto" name="produto2" >
-      <input type="text" id="produto" name="produto3" >
-      <input type="text" id="produto" name="produto4" >
-    </div>
-
-    <div class="unidade">
-      <label for="unidade">Unidade:</label>
-      <select id="unidade" name="unidade1" >
-        <option value="UN">UN</option>
-        <option value="RL">RL</option>
-        <option value="FD">FD</option>
-        <option value="KG">KG</option>
-      </select>
-      <select id="unidade" name="unidade2" >
-        <option value="UN">UN</option>
-        <option value="RL">RL</option>
-        <option value="FD">FD</option>
-        <option value="KG">KG</option>
-      </select>
-      <select id="unidade" name="unidade3" >
-        <option value="UN">UN</option>
-        <option value="RL">RL</option>
-        <option value="FD">FD</option>
-        <option value="KG">KG</option>
-      </select>
-      <select id="unidade" name="unidade4" >
-        <option value="UN">UN</option>
-        <option value="RL">RL</option>
-        <option value="FD">FD</option>
-        <option value="KG">KG</option>
-      </select>
-    </div>
-
-    <div class="quantidade">
-      <label for="quantidade">Quantidade:</label>
-      <input type="number" id="quantidade" name="quantidade1" min="1" >
-      <input type="number" id="quantidade" name="quantidade2" min="1" >
-      <input type="number" id="quantidade" name="quantidade3" min="1" >
-      <input type="number" id="quantidade" name="quantidade4" min="1" >
-    </div>
-
-    <div class="valor">
-      <label for="valor">Valor Unitário:</label>
-      <input type="number" id="valor" name="valor1" step="0.01" >
-      <input type="number" id="valor" name="valor2" step="0.01" >
-      <input type="number" id="valor" name="valor3" step="0.01" >
-      <input type="number" id="valor" name="valor4" step="0.01" >
-    </div>
-
-    <div class="ncm">
-      <label for="ncm">NCM:</label>
-      <input type="text" id="ncm" name="ncm1" >
-      <input type="text" id="ncm" name="ncm2" >
-      <input type="text" id="ncm" name="ncm3" >
-      <input type="text" id="ncm" name="ncm4" >
-    </div>
-
-    <div class="cst">
-      <label for="cst">CST:</label>
-      <input type="text" id="cst" name="cst1" >
-      <input type="text" id="cst" name="cst2" >
-      <input type="text" id="cst" name="cst3" >
-      <input type="text" id="cst" name="cst4" >
-    </div>
-
-    <div class="cfop">
-      <label for="cfop">CFOP:</label>
-      <input type="text" id="cfop" name="cfop1" >
-      <input type="text" id="cfop" name="cfop2" >
-      <input type="text" id="cfop" name="cfop3" >
-      <input type="text" id="cfop" name="cfop4" >
-    </div>
-  </div>
-
-  
-        <div class="transportes">
-            <h1>Cadastro de Transportes</h1>
-        </div>
-
-             
-                <div class="inputs">
-                   <div class="juntar">
-                    <label for="">Placa do Caminhão:</label>
-                    <input type="text" name="placa" placeholder="Placa do Caminhão">
-                    </div>
-                    <div class="juntar">
-                    <label for="">Nome do Motorista:</label>
-                    <input type="text" name="NomeMotorista" placeholder="Nome do Motorista">
-                    </div>
-                    <div class="juntar">
-                    <label for="">Container:</label>
-                    <input type="text" name="container" placeholder="Container">
-                    </div>
-                    <div class="juntar">
-                    <label for="">Navio:</label>
-                    <input type="text" name="navio" placeholder="Navio">
-                    </div>
-
-                    <div class="juntar">
-                    <label for="">Tipo:</label>
-                    <input type="text" name="tipo" placeholder="Tipo">
-                    </div>
-                    <div class="juntar">
-                    <label for="">Lacre:</label>
-                    <input type="text" name="lacre" placeholder="Lacre">
-                    </div>
-                    <div class="juntar">
-                    <label for="">Lacre SIF:</label>
-                    <input type="text" name="LacreSif" placeholder="Lacre SIF">
-                    </div>
-
-                    <div class="juntar">
-                    <label for="">IMO:</label>
-                    <input type="text" name="IMD" placeholder="IMO">
-                    </div>
-                    <div class="juntar">
-                    <label for="">N° ONU:</label>
-                    <input type="text" name="NOnu" placeholder="N° ONU">
-                    </div>
                 </div>
-                <div class="inputObs">
-                    <div>
-                    <label for="">Observação</label>
-</div>
-                    <input type="text">
-                </div>
-        
-                    <div class="enviar">
-        <input type="submit" onclick="exibirMensagem()">
-    </div>
-    
-</form>
-      
-        </div>
-                
+                <div class="form">
+                    <div class="tabela-scroll">
+                        <form action="processamento/processarcontainer.php" method="post">
+                            <div class="colunas1">
+                                <div class="numeroPedido">
+                                    <label for="num">N° pedido:</label>
+                                    <input id="num" type="number" name="npedido">
+                                </div>
+                                <div class="NomeDaEmpresa">
+                                    <label for="empresa">Empresa:</label>
+                                    <input id="empresa" type="text" name="Empresa">
+                                </div>
+                                <div class="cliente">
+                                    <label for="nomeCliente">Cliente:</label>
+                                    <input id="nomeCliente" type="text" name="Cliente">
+                                </div>
+                                <div class="telefone">
+                                    <label for="telefone">Telefone:</label>
+                                    <input id="telefone" type="number" name="Telefone">
+                                </div>
+                                <div class="cep">
+                                    <label for="cep">CEP:</label>
+                                    <input id="cep" type="number" name="CEP">
+                                </div>
+                            </div>
 
-                
+                            <div class="colunas">
+                                <div class="produto">
+                                    <label for="produto">Nome do Produto:</label>
+                                    <input type="text" id="produto" name="produto1">
+                                    <input type="text" id="produto" name="produto2">
+                                    <input type="text" id="produto" name="produto3">
+                                    <input type="text" id="produto" name="produto4">
+                                </div>
 
-                </div>
+                                <div class="unidade">
+                                    <label for="unidade">Unidade:</label>
+                                    <select id="unidade" name="unidade1">
+                                        <option value="UN">UN</option>
+                                        <option value="RL">RL</option>
+                                        <option value="FD">FD</option>
+                                        <option value="KG">KG</option>
+                                    </select>
+                                    <select id="unidade" name="unidade2">
+                                        <option value="UN">UN</option>
+                                        <option value="RL">RL</option>
+                                        <option value="FD">FD</option>
+                                        <option value="KG">KG</option>
+                                    </select>
+                                    <select id="unidade" name="unidade3">
+                                        <option value="UN">UN</option>
+                                        <option value="RL">RL</option>
+                                        <option value="FD">FD</option>
+                                        <option value="KG">KG</option>
+                                    </select>
+                                    <select id="unidade" name="unidade4">
+                                        <option value="UN">UN</option>
+                                        <option value="RL">RL</option>
+                                        <option value="FD">FD</option>
+                                        <option value="KG">KG</option>
+                                    </select>
+                                </div>
+
+                                <div class="quantidade">
+                                    <label for="quantidade">Quantidade:</label>
+                                    <input type="number" id="quantidade" name="quantidade1" min="1">
+                                    <input type="number" id="quantidade" name="quantidade2" min="1">
+                                    <input type="number" id="quantidade" name="quantidade3" min="1">
+                                    <input type="number" id="quantidade" name="quantidade4" min="1">
+                                </div>
+
+                                <div class="valor">
+                                    <label for="valor">Valor Unitário:</label>
+                                    <input type="number" id="valor" name="valor1" step="0.01">
+                                    <input type="number" id="valor" name="valor2" step="0.01">
+                                    <input type="number" id="valor" name="valor3" step="0.01">
+                                    <input type="number" id="valor" name="valor4" step="0.01">
+                                </div>
+
+                                <div class="ncm">
+                                    <label for="ncm">NCM:</label>
+                                    <input type="text" id="ncm" name="ncm1">
+                                    <input type="text" id="ncm" name="ncm2">
+                                    <input type="text" id="ncm" name="ncm3">
+                                    <input type="text" id="ncm" name="ncm4">
+                                </div>
+
+                                <div class="cst">
+                                    <label for="cst">CST:</label>
+                                    <input type="text" id="cst" name="cst1">
+                                    <input type="text" id="cst" name="cst2">
+                                    <input type="text" id="cst" name="cst3">
+                                    <input type="text" id="cst" name="cst4">
+                                </div>
+
+                                <div class="cfop">
+                                    <label for="cfop">CFOP:</label>
+                                    <input type="text" id="cfop" name="cfop1">
+                                    <input type="text" id="cfop" name="cfop2">
+                                    <input type="text" id="cfop" name="cfop3">
+                                    <input type="text" id="cfop" name="cfop4">
+                                </div>
+                            </div>
+
+
+                            <div class="transportes">
+                                <h1>Cadastro de Transportes</h1>
+                            </div>
+
+
+                            <div class="inputs">
+                                <div class="juntar">
+                                    <label for="">Placa do Caminhão:</label>
+                                    <input type="text" name="placa" placeholder="Placa do Caminhão">
+                                </div>
+                                <div class="juntar">
+                                    <label for="">Nome do Motorista:</label>
+                                    <input type="text" name="NomeMotorista" placeholder="Nome do Motorista">
+                                </div>
+                                <div class="juntar">
+                                    <label for="">Container:</label>
+                                    <input type="text" name="container" placeholder="Container">
+                                </div>
+                                <div class="juntar">
+                                    <label for="">Navio:</label>
+                                    <input type="text" name="navio" placeholder="Navio">
+                                </div>
+
+                                <div class="juntar">
+                                    <label for="">Tipo:</label>
+                                    <input type="text" name="tipo" placeholder="Tipo">
+                                </div>
+                                <div class="juntar">
+                                    <label for="">Lacre:</label>
+                                    <input type="text" name="lacre" placeholder="Lacre">
+                                </div>
+                                <div class="juntar">
+                                    <label for="">Lacre SIF:</label>
+                                    <input type="text" name="LacreSif" placeholder="Lacre SIF">
+                                </div>
+
+                                <div class="juntar">
+                                    <label for="">IMO:</label>
+                                    <input type="text" name="IMD" placeholder="IMO">
+                                </div>
+                                <div class="juntar">
+                                    <label for="">N° ONU:</label>
+                                    <input type="text" name="NOnu" placeholder="N° ONU">
+                                </div>
+                            </div>
+                            <div class="inputObs">
+                                <div>
+                                    <label for="">Observação</label>
+                                </div>
+                                <input type="text">
+                            </div>
+
+                            <div class="enviar">
+                                <input type="submit" onclick="exibirMensagem()">
+                            </div>
+
+                        </form>
+
+                    </div>
+
+
+
 
                 </div>
-    </div>
+
+            </div>
+        </div>
 
     </main>
 
 
     <script>
-    function exibirMensagem() {
-        alert('Dados enviados com sucesso!');
-    }
-</script>
+        function exibirMensagem() {
+            alert('Dados enviados com sucesso!');
+        }
+    </script>
     <script src="js/sidebar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
--- processamento/processarcontainer.php
+++ processamento/processarcontainer.php
@@ -2,176 +2,169 @@
 
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $hostname = "127.0.0.1";
-    $user = "root.Att";
-    $password = "root";
-    $database = "logistica";
+    $hostname = '127.0.0.1';
+    $user = 'root.Att';
+    $password = 'root';
+    $database = 'logistica';
 
 
 
-$conn = new mysqli($hostname, $user, $password, $database);
+    $conn = new mysqli($hostname, $user, $password, $database);
 
-// Verifique a conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
-$placa = $_POST["placa"];
-$NomeMotorista = $_POST["NomeMotorista"];
-$container=$_POST["container"];
-$navio= $_POST["navio"];
-$tipo= $_POST["tipo"];
-$lacre= $_POST["lacre"];
-$LacreSif= $_POST["LacreSif"];
-$IMD= $_POST["IMD"];
-$NOnu= $_POST["NOnu"];
-$Npedido=$_POST["npedido"];
-$Empresa=$_POST["Empresa"];
-$cliente=$_POST["cliente"];
-$telefone= $_POST["Telefone"];
-$CEP= $_POST["CEP"];
-$produto1=$_POST['produto1'];
-$produto2=$_POST['produto2'];
-$produto3=$_POST['produto3'];
-$produto4=$_POST['produto4'];
-$unidade1=$_POST['unidade1'];
-$unidade2=$_POST['unidade2'];
-$unidade3=$_POST['unidade3'];
-$unidade4=$_POST['unidade4'];
-$quantidade1=$_POST['quantidade1'];
-$quantidade2=$_POST['quantidade2'];
-$quantidade3=$_POST['quantidade3'];
-$quantidade4=$_POST['quantidade4'];
-$valor1= $_POST['valor1'];
-$valor2= $_POST['valor2'];
-$valor3= $_POST['valor3'];
-$valor4= $_POST['valor4'];
-$ncm1= $_POST['ncm1'];
-$ncm2= $_POST['ncm2'];
-$ncm3= $_POST['ncm3'];
-$ncm4= $_POST['ncm4'];
-$cst1=$_POST['cst1'];
-$cst2=$_POST['cst2'];
-$cst3= $_POST['cst3'];
-$cst4= $_POST['cst4'];
-$cfop1=$_POST['cfop1'];
-$cfop2=$_POST['cfop2'];
-$cfop3=$_POST['cfop3'];
-$cfop4= $_POST['cfop4'];
-
-
+    // Verifique a conexão
+    if ($conn->connect_error) {
+        die('Falha na conexão: ' . $conn->connect_error);
+    }
+    $placa = $_POST['placa'];
+    $NomeMotorista = $_POST['NomeMotorista'];
+    $container = $_POST['container'];
+    $navio = $_POST['navio'];
+    $tipo = $_POST['tipo'];
+    $lacre = $_POST['lacre'];
+    $LacreSif = $_POST['LacreSif'];
+    $IMD = $_POST['IMD'];
+    $NOnu = $_POST['NOnu'];
+    $npedido = $_POST['npedido'];
+    $Empresa = $_POST['Empresa'];
+    $cliente = $_POST['Cliente'];
+    $telefone = $_POST['Telefone'];
+    $CEP = $_POST['CEP'];
+    $produto1 = $_POST['produto1'];
+    $produto2 = $_POST['produto2'];
+    $produto3 = $_POST['produto3'];
+    $produto4 = $_POST['produto4'];
+    $unidade1 = $_POST['unidade1'];
+    $unidade2 = $_POST['unidade2'];
+    $unidade3 = $_POST['unidade3'];
+    $unidade4 = $_POST['unidade4'];
+    $quantidade1 = $_POST['quantidade1'];
+    $quantidade2 = $_POST['quantidade2'];
+    $quantidade3 = $_POST['quantidade3'];
+    $quantidade4 = $_POST['quantidade4'];
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $valor3 = $_POST['valor3'];
+    $valor4 = $_POST['valor4'];
+    $ncm1 = $_POST['ncm1'];
+    $ncm2 = $_POST['ncm2'];
+    $ncm3 = $_POST['ncm3'];
+    $ncm4 = $_POST['ncm4'];
+    $cst1 = $_POST['cst1'];
+    $cst2 = $_POST['cst2'];
+    $cst3 = $_POST['cst3'];
+    $cst4 = $_POST['cst4'];
+    $cfop1 = $_POST['cfop1'];
+    $cfop2 = $_POST['cfop2'];
+    $cfop3 = $_POST['cfop3'];
+    $cfop4 = $_POST['cfop4'];
 
 
 
 
 
-// Insira os dados na tabela 'processo_container'
-$sql = "INSERT INTO processo_container ( 
-    expand_more
-    edit
-    
-    -- Colunas da tabela
-    id, -- Chave primária auto-incrementada, não precisa ser informada
-    placa,
-    NomeMotorista,
-    container,
-    navio,
-    tipo,
-    lacre,
-    LacreSif,
-    Temperatura,
-    IMD,
-    NOnu,
-    situacao,
-    Npedido,
-    Empresa,
-    cliente,
-    Telefone,
-    CEP,
-    produto1,
-    produto2,
-    produto3,
-    produto4,
-    unidade1,
-    unidade2,
-    unidade3,
-    unidade4,
-    quantidade1,
-    quantidade2,
-    quantidade3,
-    quantidade4,
-    valor1,
-    valor2,
-    valor3,
-    valor4,
-    ncm1,
-    ncm2,
-    ncm3,
-    ncm4,
-    cst1,
-    cst2,
-    cst3,
-    cst4,
-    cfop1,
-    cfop2,
-    cfop3,
-    cfop4
-  )
-  VALUES (
-    -- Valores das variáveis ​​do PHP
-    '$placa',
-    '$NomeMotorista',
-    '$container',
-    '$navio',
-    '$tipo',
-    '$lacre',
-    '$LacreSif',
-    '$Temperatura',
-    '$IMD',
-    '$NOnu',
-    '$situacao',
-    '$Npedido',
-    '$Empresa',
-    '$cliente',
-    '$Telefone',
-    '$CEP',
-    '$produto1',
-    '$produto2',
-    '$produto3',
-    '$produto4',
-    '$unidade1',
-    '$unidade2',
-    '$unidade3',
-    '$unidade4',
-    '$quantidade1',
-    '$quantidade2',
-    '$quantidade3',
-    '$quantidade4',
-    '$valor1',
-    '$valor2',
-    '$valor3',
-    '$valor4',
-    '$ncm1',
-    '$ncm2',
-    '$ncm3',
-    '$ncm4',
-    '$cst1',
-    '$cst2',
-    '$cst3',
-    '$cst4',
-    '$cfop1',
-    '$cfop2',
-    '$cfop3',
-    '$cfop4',
-  );";
-  
-if ($conn->query($sql) === TRUE) {
-    echo "Dados inseridos com sucesso!";
-    header('location:../containerP.php');
-} else {
-    echo "Erro ao inserir dados: " . $conn->error;
-}
 
-$conn->close();
+
+    // Insira os dados na tabela 'processo_container'
+    $sql = 'INSERT INTO processo_container (
+        placa,
+        NomeMotorista,
+        container,
+        navio,
+        tipo,
+        lacre,
+        LacreSif,
+        IMD,
+        NOnu,
+        situacao,
+        npedido,
+        Empresa,
+        cliente,
+        Telefone,
+        CEP,
+        produto1,
+        produto2,
+        produto3,
+        produto4,
+        unidade1,
+        unidade2,
+        unidade3,
+        unidade4,
+        quantidade1,
+        quantidade2,
+        quantidade3,
+        quantidade4,
+        valor1,
+        valor2,
+        valor3,
+        valor4,
+        ncm1,
+        ncm2,
+        ncm3,
+        ncm4,
+        cst1,
+        cst2,
+        cst3,
+        cst4,
+        cfop1,
+        cfop2,
+        cfop3,
+        cfop4
+    )
+    VALUES (
+        '$placa',
+        '$NomeMotorista',
+        '$container',
+        '$navio',
+        '$tipo',
+        '$lacre',
+        '$LacreSif',
+        '$IMD',
+        '$NOnu',
+        'enviado', 
+        '$npedido',
+        '$Empresa',
+        '$cliente',
+        '$Telefone',
+        '$CEP',
+        '$produto1',
+        '$produto2',
+        '$produto3',
+        '$produto4',
+        '$unidade1',
+        '$unidade2',
+        '$unidade3',
+        '$unidade4',
+        '$quantidade1',
+        '$quantidade2',
+        '$quantidade3',
+        '$quantidade4',
+        '$valor1',
+        '$valor2',
+        '$valor3',
+        '$valor4',
+        '$ncm1',
+        '$ncm2',
+        '$ncm3',
+        '$ncm4',
+        '$cst1',
+        '$cst2',
+        '$cst3',
+        '$cst4',
+        '$cfop1',
+        '$cfop2',
+        '$cfop3',
+        '$cfop4'
+    );
+    ;
+
+    if ($conn->query($sql) === TRUE) {
+        echo 'Dados inseridos com sucesso!';
+        header('location:../containerP.php');
+    } else {
+        echo 'Erro ao inserir dados: ' . $conn->error;
+    }
+
+    $conn->close();
 
 }
 
