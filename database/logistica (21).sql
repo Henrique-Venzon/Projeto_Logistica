b5f8e4ad34080f962651f371647df52f190fef36
Matheus Yan <reismatheusyan83@gmail.com>
2024-05-16 11:12:21 -0300

    Update logistica (21).sql

--- database/logistica (21).sql
+++ database/logistica (21).sql
@@ -1,33 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Tempo de geração: 16/05/2024 às 11:35
--- Versão do servidor: 8.0.36
--- Versão do PHP: 8.2.13
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `logistica`
---
 CREATE DATABASE IF NOT EXISTS `logistica` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `logistica`;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `aluno`
---
 DROP TABLE IF EXISTS `aluno`;
 CREATE TABLE IF NOT EXISTS `aluno` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -37,11 +12,6 @@ CREATE TABLE IF NOT EXISTS `aluno` (
   PRIMARY KEY (`id`),
   KEY `turma_id` (`turma_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `aluno`
---
-
 INSERT INTO `aluno` (`id`, `username`, `password`, `turma_id`) VALUES
 (1, 'root.Att', 'root', 1),
 (12, 'teste', 'teste', 1),
@@ -49,13 +19,6 @@ INSERT INTO `aluno` (`id`, `username`, `password`, `turma_id`) VALUES
 (95, 'Aluno 2', 'blFm', 2),
 (97, 'Aluno 1', 'Wd7A', 3),
 (98, 'Aluno 2', '2Zcj', 3);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `armazem_limite`
---
-
 DROP TABLE IF EXISTS `armazem_limite`;
 CREATE TABLE IF NOT EXISTS `armazem_limite` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -63,13 +26,6 @@ CREATE TABLE IF NOT EXISTS `armazem_limite` (
   `limite_maximo` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `atividade_concluida`
---
-
 DROP TABLE IF EXISTS `atividade_concluida`;
 CREATE TABLE IF NOT EXISTS `atividade_concluida` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -95,21 +51,9 @@ CREATE TABLE IF NOT EXISTS `atividade_concluida` (
   KEY `id_aluno` (`id_aluno`),
   KEY `id_transporte` (`id_transporte`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `atividade_concluida`
---
-
 INSERT INTO `atividade_concluida` (`id`, `id_transporte`, `id_turma`, `id_aluno`, `sem_lona`, `avariana_lateral_direita`, `sem_cabo_de_energia`, `avaria_no_teto`, `avaria_na_frente`, `sem_lacre`, `adesivos_avariados`, `excesso_de_altura`, `excesso_na_direita`, `excesso_na_esquerda`, `excesso_na_frente`, `painel_avariado`, `avariana_na_lateral_esquerda`, `container_bem_desgastado`) VALUES
 (18, 2, 1, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 (19, 1, 1, 12, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `atividade_turma`
---
-
 DROP TABLE IF EXISTS `atividade_turma`;
 CREATE TABLE IF NOT EXISTS `atividade_turma` (
   `id_atividade` int NOT NULL,
@@ -118,13 +62,6 @@ CREATE TABLE IF NOT EXISTS `atividade_turma` (
   PRIMARY KEY (`id_atividade`,`id_turma`),
   KEY `id_turma` (`id_turma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `nota_fiscal_criadas`
---
-
 DROP TABLE IF EXISTS `nota_fiscal_criadas`;
 CREATE TABLE IF NOT EXISTS `nota_fiscal_criadas` (
   `id_atividade` int NOT NULL,
@@ -133,20 +70,8 @@ CREATE TABLE IF NOT EXISTS `nota_fiscal_criadas` (
   PRIMARY KEY (`id_atividade`,`id_turma`),
   KEY `id_turma` (`id_turma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `nota_fiscal_criadas`
---
-
 INSERT INTO `nota_fiscal_criadas` (`id_atividade`, `id_turma`, `data_hora_envio`) VALUES
 (2, 1, '2024-05-16 08:34:23');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `produto`
---
-
 DROP TABLE IF EXISTS `produto`;
 CREATE TABLE IF NOT EXISTS `produto` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -158,20 +83,8 @@ CREATE TABLE IF NOT EXISTS `produto` (
   PRIMARY KEY (`id`),
   KEY `turma_id` (`turma_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `produto`
---
-
 INSERT INTO `produto` (`id`, `nome_produto`, `preco`, `UN`, `Quantidade`, `turma_id`) VALUES
 (1, 'Tesoura', 5.00, 'UN', 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `professor`
---
-
 DROP TABLE IF EXISTS `professor`;
 CREATE TABLE IF NOT EXISTS `professor` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -181,20 +94,8 @@ CREATE TABLE IF NOT EXISTS `professor` (
   PRIMARY KEY (`id`),
   KEY `turma_id` (`turma_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `professor`
---
-
 INSERT INTO `professor` (`id`, `username`, `password`, `turma_id`) VALUES
 (1, 'teste', 'teste', 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `transporte`
---
-
 DROP TABLE IF EXISTS `transporte`;
 CREATE TABLE IF NOT EXISTS `transporte` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -246,58 +147,21 @@ CREATE TABLE IF NOT EXISTS `transporte` (
   PRIMARY KEY (`id`),
   KEY `turma_id` (`turma_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `transporte`
---
-
 INSERT INTO `transporte` (`id`, `placa`, `NomeMotorista`, `container`, `navio`, `tipo`, `lacre`, `LacreSif`, `IMD`, `NOnu`, `situacao`, `npedido`, `temperatura`, `Empresa`, `cliente`, `Telefone`, `CEP`, `produto1`, `produto2`, `produto3`, `produto4`, `unidade1`, `unidade2`, `unidade3`, `unidade4`, `quantidade1`, `quantidade2`, `quantidade3`, `quantidade4`, `valor1`, `valor2`, `valor3`, `valor4`, `ncm1`, `ncm2`, `ncm3`, `ncm4`, `cst1`, `cst2`, `cst3`, `cst4`, `cfop1`, `cfop2`, `cfop3`, `cfop4`, `turma_id`) VALUES
 (5, '231QAC', 'Portifólio Matheus Yan', '2231', 'KL2332', '22G1', 'ty223At', 41231, '1', 2546, 'enviado', '4124', 235, 'a', 'a', '4799684', '88380000', 'tesoura', 'apk', '', '', 'UN', 'FD', ' ', ' ', 5, 23, 0, 0, 5.00, 4.00, 0.00, 0.00, 231, 52143, 0, 0, 231, 2314, 0, 0, 123, 2313, 0, 0, 2);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `turma`
---
-
 DROP TABLE IF EXISTS `turma`;
 CREATE TABLE IF NOT EXISTS `turma` (
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=670 DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `turma`
---
-
 INSERT INTO `turma` (`id`) VALUES
 (1),
 (2),
 (3);
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `aluno`
---
 ALTER TABLE `aluno`
   ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`turma_id`) REFERENCES `turma` (`id`);
-
---
--- Restrições para tabelas `produto`
---
 ALTER TABLE `produto`
   ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`turma_id`) REFERENCES `turma` (`id`);
-
---
--- Restrições para tabelas `professor`
---
 ALTER TABLE `professor`
   ADD CONSTRAINT `professor_ibfk_1` FOREIGN KEY (`turma_id`) REFERENCES `turma` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
