f8b51bc479551e975a1e82e33e73bd8c737745e4
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-05-16 08:50:24 -0300

    d

--- js/ver.js
+++ js/ver.js
@@ -1,6 +1,17 @@
+
+
+document.getElementById('v').addEventListener('click', function() {
+    window.location.href = 'verPedido.php'; 
+});
+
+
 document.getElementById('ver').addEventListener('click', function() {
     window.location.href = 'nPedido.php'; 
 });
+
+
+
 function nota() {
     alert('Nota fiscal criada!');
 }
+
