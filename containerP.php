d6489cbcd57ed4338c4cd9627820585a91088f2c
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2024-05-21 11:12:56 -0300

    save

--- containerP.php
+++ containerP.php
@@ -54,15 +54,22 @@ if (!isset($_SESSION['turma'])) {
                 <div class="form">
                     <div class="tabela-scroll">
                         <div class="presets">
-                        <button onclick="setMultipleInputValues({'empresa': 'Portonave', 'nomeCliente': 'Matheus Yan', 'telefone':'4739406838','cep':'88370904','produto':'Teclado','produto2':'Mouse','unidade':'UN','unidade2':'UN','quantidade':'10','quantidade2':'15','valor':'15.00','valor2':'8.00'})">Preset 1</button>
-                        <button onclick="setMultipleInputValues({'empresa': 'MultiLog', 'nomeCliente': 'Luan Pereira', 'telefone':'4789426155','cep':'23812310','produto':'Motor','produto2':'Óleo Diesel','unidade':'UN','unidade2':'L','quantidade':'15','quantidade2':'30','valor':'2800.00','valor2':'2.40'})">Preset 2</button>
-                        <button onclick="setMultipleInputValues({'empresa': 'ARXO', 'nomeCliente': 'Henrique Venzon', 'telefone':'4791296865','cep':'88318481','produto':'Camisa','produto2':'Moletom','unidade':'UN','unidade2':'UN','quantidade':'30','quantidade2':'20','valor':'20.00','valor2':'24.00'})">Preset 3</button>
+                            <button
+                                onclick="setMultipleInputValues({'empresa': 'Portonave', 'nomeCliente': 'Matheus Yan', 'telefone':'4739406838','cep':'88370904','produto':'Teclado','produto2':'Mouse','unidade':'UN','unidade2':'UN','quantidade':'10','quantidade2':'15','valor':'15.00','valor2':'8.00'})">Preset
+                                1</button>
+                            <button
+                                onclick="setMultipleInputValues({'empresa': 'MultiLog', 'nomeCliente': 'Luan Pereira', 'telefone':'4789426155','cep':'23812310','produto':'Motor','produto2':'Óleo Diesel','unidade':'UN','unidade2':'L','quantidade':'15','quantidade2':'30','valor':'2800.00','valor2':'2.40'})">Preset
+                                2</button>
+                            <button
+                                onclick="setMultipleInputValues({'empresa': 'ARXO', 'nomeCliente': 'Henrique Venzon', 'telefone':'4791296865','cep':'88318481','produto':'Camisa','produto2':'Moletom','unidade':'UN','unidade2':'UN','quantidade':'30','quantidade2':'20','valor':'20.00','valor2':'24.00'})">Preset
+                                3</button>
                         </div>
                         <form action="processamento/carga.php" method="post">
                             <div class="colunas1">
                                 <div class="numeroPedido">
                                     <label for="num">N° pedido:</label>
-                                    <input id="num" type="number" name="npedido" min="0" required placeholder="Obrigatório">
+                                    <input id="num" type="number" name="npedido" min="0" required
+                                        placeholder="Obrigatório">
                                 </div>
                                 <div class="NomeDaEmpresa">
                                     <label for="empresa">Empresa:</label>
@@ -70,11 +77,13 @@ if (!isset($_SESSION['turma'])) {
                                 </div>
                                 <div class="cliente">
                                     <label for="nomeCliente">Cliente:</label>
-                                    <input id="nomeCliente" type="text" name="Cliente" required placeholder="Obrigatório">
+                                    <input id="nomeCliente" type="text" name="Cliente" required
+                                        placeholder="Obrigatório">
                                 </div>
                                 <div class="telefone">
                                     <label for="telefone">Telefone:</label>
-                                    <input id="telefone" type="number" name="Telefone" required placeholder="Obrigatório">
+                                    <input id="telefone" type="number" name="Telefone" required
+                                        placeholder="Obrigatório">
                                 </div>
                                 <div class="cep">
                                     <label for="cep">CEP:</label>
@@ -93,14 +102,14 @@ if (!isset($_SESSION['turma'])) {
 
                                 <div class="unidade">
                                     <label for="unidade">Unidade:</label>
-                                    <select id="unidade" name="unidade1" required >
+                                    <select id="unidade" name="unidade1" required>
                                         <option value="UN">UN</option>
                                         <option value="RL">RL</option>
                                         <option value="FD">FD</option>
                                         <option value="KG">KG</option>
                                         <option value="L">L</option>
                                     </select>
-                                    <select id="unidade2" name="unidade2" >
+                                    <select id="unidade2" name="unidade2">
                                         <?php // <?php if ($produto2==' ') print "<option value=' '> </option>" ?>
                                         <option value=" "> </option>
                                         <option value="UN">UN</option>
@@ -109,7 +118,7 @@ if (!isset($_SESSION['turma'])) {
                                         <option value="KG">KG</option>
                                         <option value="L">L</option>
                                     </select>
-                                    <select id="unidade3" name="unidade3" >
+                                    <select id="unidade3" name="unidade3">
                                         <option value=" "> </option>
                                         <option value="UN">UN</option>
                                         <option value="RL">RL</option>
@@ -117,7 +126,7 @@ if (!isset($_SESSION['turma'])) {
                                         <option value="KG">KG</option>
                                         <option value="L">L</option>
                                     </select>
-                                    <select id="unidade4" name="unidade4" >
+                                    <select id="unidade4" name="unidade4">
                                         <option value=" "> </option>
                                         <option value="UN">UN</option>
                                         <option value="RL">RL</option>
@@ -129,15 +138,20 @@ if (!isset($_SESSION['turma'])) {
 
                                 <div class="quantidade">
                                     <label for="quantidade">Quantidade:</label>
-                                    <input type="number" id="quantidade" name="quantidade1" min="0" required placeholder="Obrigatório">
-                                    <input type="number" id="quantidade2" name="quantidade2" min="0" placeholder="Opcional">
-                                    <input type="number" id="quantidade3" name="quantidade3" min="0" placeholder="Opcional">
-                                    <input type="number" id="quantidade4" name="quantidade4" min="0" placeholder="Opcional">
+                                    <input type="number" id="quantidade" name="quantidade1" min="0" required
+                                        placeholder="Obrigatório">
+                                    <input type="number" id="quantidade2" name="quantidade2" min="0"
+                                        placeholder="Opcional">
+                                    <input type="number" id="quantidade3" name="quantidade3" min="0"
+                                        placeholder="Opcional">
+                                    <input type="number" id="quantidade4" name="quantidade4" min="0"
+                                        placeholder="Opcional">
                                 </div>
 
                                 <div class="valor">
                                     <label for="valor">Valor Unitário:</label>
-                                    <input type="number" id="valor" name="valor1" step="0.01" required placeholder="Obrigatório">
+                                    <input type="number" id="valor" name="valor1" step="0.01" required
+                                        placeholder="Obrigatório">
                                     <input type="number" id="valor2" name="valor2" step="0.01" placeholder="Opcional">
                                     <input type="number" id="valor3" name="valor3" step="0.01" placeholder="Opcional">
                                     <input type="number" id="valor4" name="valor4" step="0.01" placeholder="Opcional">
@@ -185,7 +199,7 @@ if (!isset($_SESSION['turma'])) {
                                 </div>
                                 <div class="juntar">
                                     <label for="">Temperatura:</label>
-                                    <input type="text" name="temperatura" placeholder="Opcional" >
+                                    <input type="text" name="temperatura" placeholder="Opcional">
                                 </div>
                                 <div class="juntar">
                                     <label for="nome">Nome do Motorista:</label>
@@ -210,16 +224,16 @@ if (!isset($_SESSION['turma'])) {
                                 </div>
                                 <div class="juntar">
                                     <label for="">Lacre SIF:</label>
-                                    <input type="text" name="LacreSif" placeholder="Opciona" >
+                                    <input type="text" name="LacreSif" placeholder="Opciona">
                                 </div>
 
                                 <div class="juntar">
                                     <label for="">IMO:</label>
-                                    <input type="text" name="IMO" placeholder="Opcional" >
+                                    <input type="text" name="IMD" placeholder="Opcional">
                                 </div>
                                 <div class="juntar">
                                     <label for="">N° ONU:</label>
-                                    <input type="text" name="NOnu" placeholder="Opcional" >
+                                    <input type="text" name="NOnu" placeholder="Opcional">
                                 </div>
                             </div>
                             <div class="inputObs">
@@ -244,9 +258,6 @@ if (!isset($_SESSION['turma'])) {
     </main>
 
 
-    <script>
-
-    </script>
     <script src="js/sidebar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
@@ -254,27 +265,7 @@ if (!isset($_SESSION['turma'])) {
 </body>
 
 </html>
-<script>
-        function exibirMensagem() {
-            const numPedido = document.getElementById('num').value;
-            const empresa = document.getElementById('empresa').value;
-            const nomeCliente = document.getElementById('nomeCliente').value;
-            const telefone = document.getElementById('telefone').value;
-            const cep = document.getElementById('cep').value;
-            const produto = document.getElementById('produto').value;
-            const unidade = document.getElementById('unidade').value;
-            const valor = document.getElementById('valor').value;
-          
 
-            if (numPedido && empresa && nomeCliente && telefone && cep && produto && unidade && valor && nome && container && navio && tipo && lacre) {
-                alert('Dados enviados com sucesso!');
-            } else {
-                alert('Erro no envio. Preencha todos os campos obrigatórios.');
-            }
-        }
-
-
-    </script>x
 <script>
     function setMultipleInputValues(values) {
         for (var id in values) {
