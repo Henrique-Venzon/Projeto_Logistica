a64306e0d7b621beda0bd28d1486a78a4cae6702
Matheus Yan <reismatheusyan83@gmail.com>
2024-05-22 20:24:49 -0300

    Update docas.php

--- docas.php
+++ docas.php
@@ -4,10 +4,6 @@ if(!isset($_SESSION['id'])){
     header("Location: index.php");
     exit;
 }
-if (($_SESSION['tipo_login'] != 'professor')) {
-    header("Location: index.php");
-    exit;
-}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_SESSION['turma'] = $_POST['turma'];}
 ?>
