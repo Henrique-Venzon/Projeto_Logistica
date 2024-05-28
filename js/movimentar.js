ebe9729ccd66e4ae5e4233543d50b5b9ec97ffa2
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-05-28 11:10:19 -0300

    muita coisa pt3

--- js/movimentar.js
+++ js/movimentar.js
@@ -1,12 +1,12 @@
 document.getElementById('doca1').addEventListener('click', function() {
-    window.location.href = 'verPedido.php'; 
+    window.location.href = 'movimentarD1.php'; 
 });
 document.getElementById('doca2').addEventListener('click', function() {
-    window.location.href = 'verPedido.php'; 
+    window.location.href = 'movimentarD2.php'; 
 });
 document.getElementById('doca3').addEventListener('click', function() {
-    window.location.href = 'verPedido.php'; 
+    window.location.href = 'movimentarD3.php'; 
 });
 document.getElementById('doca4').addEventListener('click', function() {
-    window.location.href = 'verPedido.php'; 
+    window.location.href = 'movimentarD4.php'; 
 });
---
+++ movimentarD1.php
@@ -0,0 +1,156 @@
+<?php
+$id_aluno = 1;
+session_start();
+if (!isset($_SESSION['id'])) {
+    header("Location: index.php");
+    exit;
+}
+if (!isset($_SESSION['turma'])) {
+    header("Location: index.php");
+    exit;
+}
+$turma = $_SESSION['turma']
+    ?>
+<!DOCTYPE html>
+
+<head>
+    <meta name="vierport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/amem.svg">
+
+    <meta charset="utf-8">
+    <title><?php
+    $tituloPag = 'Movimentação';
+    echo "$tituloPag";
+    ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="css/movDocas.css">
+</head>
+
+<body>
+    <?php
+    include 'include/header.php'
+        ?>
+    <main>
+        <?php
+        include 'include/menuLateral.php'
+            ?>
+
+        <div class="DivDireita">
+            <div class="table-inputs">
+                    <div class="txtCont">
+                        <h1>Doca 1</h1>
+                    </div>
+                    <div class="flex">
+                    <form action="">
+                    <h1 class="pegar">Pegar</h1>
+                    <?php        
+                                    $servername = "localhost";
+                                    $username = "root.Att";
+                                    $password = "root";
+                                    $dbname = "logistica";
+
+                                    // Create connection
+                                    $conn = new mysqli($servername, $username, $password, $dbname);     
+
+                                    $sql = "SELECT * FROM movimentacao where id_doca='1'";
+
+                                    $res = $conn->query($sql);
+
+                                    $qtd = $res->num_rows;
+
+                                    if($qtd > 0){
+                                        print "<div class=\"tabela-scroll\">";
+                                        print "<table class='table' >";
+                                        print "<tr>";
+                                        print "<th>Produto</th>";
+                                        print "<th>Quantidade</th>";
+                                        print "<th>Posição</th>";
+                                        print "<th>Pegar</th>";
+                                        print "</tr>";
+                                        
+                                            while($row = $res->fetch_object()){
+                                                print "<tr>";
+                                                print "<td style=\"border-right:1px solid black;\">".$row->nome_produto."</td>";
+                                                print "<td style=\"border-right:1px solid black;\">".$row->quantidade_enviada."</td>";
+                                                print "<td style=\"border-right:1px solid black;\">".$row->posicao."</td>";
+                                                print "<td><input class=\"custom-checkbox\" type=\"checkbox\"></td>";
+
+                                                print "</tr>";
+                                            }
+                                            print "</table>";
+                                            print "</div>";
+
+                                    }else{
+                                        print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
+                                    }
+                                    ?>
+                                <DIV class="buttonEnviar">
+                                    <button>enviar</button>
+                                </DIV>
+                                </form>
+                    <form action="">
+                    <h1 class="pegar">Finalizar</h1>
+
+                    <?php        
+                                    $servername = "localhost";
+                                    $username = "root.Att";
+                                    $password = "root";
+                                    $dbname = "logistica";
+
+                                    // Create connection
+                                    $conn = new mysqli($servername, $username, $password, $dbname);     
+
+                                    $sql = "SELECT * FROM movimentacao where id_doca='1'";
+
+                                    $res = $conn->query($sql);
+
+                                    $qtd = $res->num_rows;
+
+                                    if($qtd > 0){
+                                        print "<div class=\"tabela-scroll\">";
+                                        print "<table class='table' >";
+                                        print "<tr>";
+                                        print "<th>Produto</th>";
+                                        print "<th>Quantidade</th>";
+                                        print "<th>Posição</th>";
+                                        print "<th>Pegar</th>";
+                                        print "</tr>";
+                                        
+                                            while($row = $res->fetch_object()){
+                                                print "<tr>";
+                                                print "<td style=\"border-right:1px solid black;\">".$row->nome_produto."</td>";
+                                                print "<td style=\"border-right:1px solid black;\">".$row->quantidade_enviada."</td>";
+                                                print "<td style=\"border-right:1px solid black;\">".$row->posicao."</td>";
+                                                print "<td><input class=\"custom-checkbox\" type=\"checkbox\"></td>";
+
+                                                print "</tr>";
+                                            }
+                                            print "</table>";
+                                            print "</div>";
+
+                                    }else{
+                                        print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
+                                    }
+                                    ?>
+                                <DIV class="buttonEnviar">
+                                    <button>enviar</button>
+                                </DIV>
+                                </form>
+            </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="js/movimentar.js"></script>
+    <script src="js/sidebar.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
