260ca16de80319f77dc6d636002e406f45b3a75c
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-05-28 09:14:35 -0300

    .

---
+++ js/movimentar.js
@@ -0,0 +1,12 @@
+document.getElementById('doca1').addEventListener('click', function() {
+    window.location.href = 'verPedido.php'; 
+});
+document.getElementById('doca2').addEventListener('click', function() {
+    window.location.href = 'verPedido.php'; 
+});
+document.getElementById('doca3').addEventListener('click', function() {
+    window.location.href = 'verPedido.php'; 
+});
+document.getElementById('doca4').addEventListener('click', function() {
+    window.location.href = 'verPedido.php'; 
+});
