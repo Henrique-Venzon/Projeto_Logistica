cb53c9b2395d9229c00bf0acbaedc09ad9554405
Matheus Yan <reismatheusyan83@gmail.com>
2024-06-04 10:15:44 -0300

    .

--- movimentacaoDoca.php
+++ movimentacaoDoca.php
@@ -42,166 +42,175 @@ $turma = $_SESSION['turma']
 
         <div class="DivDireita">
             <div class="table-inputs">
-                    <div class="txtCont">
-                        <h1>Movimentação</h1>
-                    </div>
-                        <div class="container">
-                            <div class="espacamento">
-                                <div class="cardsDocas" id="doca1">
-                                <h1>Doca 1</h1>
-                              <?php        
-                                    $servername = "localhost";
-                                    $username = "root.Att";
-                                    $password = "root";
-                                    $dbname = "logistica";
+                <div class="txtCont">
+                    <h1>Movimentação</h1>
+                </div>
+                <div class="container">
+                    <div class="espacamento">
+                        <div class="cardsDocas" id="doca1">
+                            <h1>Doca 1</h1>
+                            <?php
+                            $servername = "localhost";
+                            $username = "root.Att";
+                            $password = "root";
+                            $dbname = "logistica";
 
-                                    // Create connection
-                                    $conn = new mysqli($servername, $username, $password, $dbname);     
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
 
-                                    $sql = "SELECT * FROM docas where id_doca='1'";
+                            if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                            }
 
-                                    $res = $conn->query($sql);
+                            // Modificado para incluir junção com a tabela 'carga' e condição na coluna 'situacao'
+                            $sql = "
+                                    SELECT docas.id_carga 
+                                    FROM docas 
+                                    JOIN vistoriado ON docas.id_carga = vistoriado.id 
+                                    WHERE docas.id_doca = '1' AND vistoriado.situacao = 'Vistoriado'
+                                    ";
 
-                                    $qtd = $res->num_rows;
+                            $res = $conn->query($sql);
 
-                                    if($qtd > 0){
-                                        print "<div class=\"tabela-scroll\">";
-                                        print "<table class='table' >";
+                            $qtd = $res->num_rows;
 
-                                        print "<tr>";
-                                                print "<th style=\"border-left:none;\">N° Pedido</th>";
-                                                print "</tr>";
-                                        
-                                            while($row = $res->fetch_object()){
-                                                print "<tr>";
-                                                print "<td style=\"border-right:1px solid black;\">".$row->id_carga."</td>";
-                                                print "</tr>";
-                                            }
-                                            print "</table>";
-                                            print "</div>";
+                            if ($qtd > 0) {
+                                print "<div class=\"tabela-scroll\">";
+                                print "<table class='table' >";
+                                print "<tr>";
+                                print "<th style=\"border-left:none;\">N° Pedido</th>";
+                                print "</tr>";
 
-                                    }else{
-                                        print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
-                                    }
-                                    ?>
-                                </div>
-                                <div class="cardsDocas" id="doca2">
-                                <h1>Doca 2</h1>
-                                <?php        
-                                    $servername = "localhost";
-                                    $username = "root.Att";
-                                    $password = "root";
-                                    $dbname = "logistica";
+                                while ($row = $res->fetch_object()) {
+                                    print "<tr>";
+                                    print "<td style=\"border-right:1px solid black;\">" . $row->id_carga . "</td>";
+                                    print "</tr>";
+                                }
+                                print "</table>";
+                                print "</div>";
+                            } else {
+                                print "<p class='alert alert-danger'>Não encontrou nenhum pedido nas docas.</p>";
+                            }
+                            ?>
 
-                                    // Create connection
-                                    $conn = new mysqli($servername, $username, $password, $dbname);     
-
-                                    $sql = "SELECT * FROM docas where id_doca='2'";
-
-                                    $res = $conn->query($sql);
-
-                                    $qtd = $res->num_rows;
-
-                                    if($qtd > 0){
-                                        print "<div class=\"tabela-scroll\">";
-                                        print "<table class='table' >";
-
-                                        print "<tr>";
-                                                print "<th style=\"border-left:none;\">N° Pedido</th>";
-                                                print "</tr>";
-                                        
-                                            while($row = $res->fetch_object()){
-                                                print "<tr>";
-                                                print "<td style=\"border-right:1px solid black;\">".$row->id_carga."</td>";
-                                                print "</tr>";
-                                            }
-                                            print "</table>";
-                                            print "</div>";
-
-                                    }else{
-                                        print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
-                                    }
-                                    ?>
-                                </div>
-                                <div class="cardsDocas" id="doca3">
-                                <h1>Doca 3</h1>
-                                <?php        
-                                    $servername = "localhost";
-                                    $username = "root.Att";
-                                    $password = "root";
-                                    $dbname = "logistica";
-
-                                    // Create connection
-                                    $conn = new mysqli($servername, $username, $password, $dbname);     
-
-                                    $sql = "SELECT * FROM docas where id_doca='3'";
-
-                                    $res = $conn->query($sql);
-
-                                    $qtd = $res->num_rows;
-
-                                    if($qtd > 0){
-                                        print "<div class=\"tabela-scroll\">";
-                                        print "<table class='table' >";
-
-                                        print "<tr>";
-                                                print "<th style=\"border-left:none;\">N° Pedido</th>";
-                                                print "</tr>";
-                                        
-                                            while($row = $res->fetch_object()){
-                                                print "<tr>";
-                                                print "<td style=\"border-right:1px solid black;\">".$row->id_carga."</td>";
-                                                print "</tr>";
-                                            }
-                                            print "</table>";
-                                            print "</div>";
-
-                                    }else{
-                                        print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
-                                    }
-                                    ?>
-                                </div>
-                                <div class="cardsDocas" id="doca4">
-                                <h1>Doca 4</h1>
-                                <?php        
-                                    $servername = "localhost";
-                                    $username = "root.Att";
-                                    $password = "root";
-                                    $dbname = "logistica";
-
-                                    // Create connection
-                                    $conn = new mysqli($servername, $username, $password, $dbname);     
-
-                                    $sql = "SELECT * FROM docas where id_doca='4'";
-
-                                    $res = $conn->query($sql);
-
-                                    $qtd = $res->num_rows;
-
-                                    if($qtd > 0){
-                                        print "<div class=\"tabela-scroll\">";
-                                        print "<table class='table' >";
-
-                                        print "<tr>";
-                                                print "<th style=\"border-left:none;\">N° Pedido</th>";
-                                                print "</tr>";
-                                        
-                                            while($row = $res->fetch_object()){
-                                                print "<tr>";
-                                                print "<td style=\"border-right:1px solid black;\">".$row->id_carga."</td>";
-                                                print "</tr>";
-                                            }
-                                            print "</table>";
-                                            print "</div>";
-
-                                    }else{
-                                        print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
-                                    }
-                                    ?>
-                                </div>
-                            </div>
                         </div>
-                
+                        <div class="cardsDocas" id="doca2">
+                            <h1>Doca 2</h1>
+                            <?php
+                            $servername = "localhost";
+                            $username = "root.Att";
+                            $password = "root";
+                            $dbname = "logistica";
+
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+
+                            $sql = "SELECT * FROM docas where id_doca='2'";
+
+                            $res = $conn->query($sql);
+
+                            $qtd = $res->num_rows;
+
+                            if ($qtd > 0) {
+                                print "<div class=\"tabela-scroll\">";
+                                print "<table class='table' >";
+
+                                print "<tr>";
+                                print "<th style=\"border-left:none;\">N° Pedido</th>";
+                                print "</tr>";
+
+                                while ($row = $res->fetch_object()) {
+                                    print "<tr>";
+                                    print "<td style=\"border-right:1px solid black;\">" . $row->id_carga . "</td>";
+                                    print "</tr>";
+                                }
+                                print "</table>";
+                                print "</div>";
+
+                            } else {
+                                print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
+                            }
+                            ?>
+                        </div>
+                        <div class="cardsDocas" id="doca3">
+                            <h1>Doca 3</h1>
+                            <?php
+                            $servername = "localhost";
+                            $username = "root.Att";
+                            $password = "root";
+                            $dbname = "logistica";
+
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+
+                            $sql = "SELECT * FROM docas where id_doca='3'";
+
+                            $res = $conn->query($sql);
+
+                            $qtd = $res->num_rows;
+
+                            if ($qtd > 0) {
+                                print "<div class=\"tabela-scroll\">";
+                                print "<table class='table' >";
+
+                                print "<tr>";
+                                print "<th style=\"border-left:none;\">N° Pedido</th>";
+                                print "</tr>";
+
+                                while ($row = $res->fetch_object()) {
+                                    print "<tr>";
+                                    print "<td style=\"border-right:1px solid black;\">" . $row->id_carga . "</td>";
+                                    print "</tr>";
+                                }
+                                print "</table>";
+                                print "</div>";
+
+                            } else {
+                                print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
+                            }
+                            ?>
+                        </div>
+                        <div class="cardsDocas" id="doca4">
+                            <h1>Doca 4</h1>
+                            <?php
+                            $servername = "localhost";
+                            $username = "root.Att";
+                            $password = "root";
+                            $dbname = "logistica";
+
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+
+                            $sql = "SELECT * FROM docas where id_doca='4'";
+
+                            $res = $conn->query($sql);
+
+                            $qtd = $res->num_rows;
+
+                            if ($qtd > 0) {
+                                print "<div class=\"tabela-scroll\">";
+                                print "<table class='table' >";
+
+                                print "<tr>";
+                                print "<th style=\"border-left:none;\">N° Pedido</th>";
+                                print "</tr>";
+
+                                while ($row = $res->fetch_object()) {
+                                    print "<tr>";
+                                    print "<td style=\"border-right:1px solid black;\">" . $row->id_carga . "</td>";
+                                    print "</tr>";
+                                }
+                                print "</table>";
+                                print "</div>";
+
+                            } else {
+                                print "<p class='alert alert-danger'>Não encrontrou nenhum pedido nas docas.</p>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </main>
--- processamento/processar_movimentacaoD2.php
+++ processamento/processar_movimentacaoD2.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$turma = $_SESSION['turma']; // Variável nova adicionada
 if (!isset($_SESSION['id']) || !isset($_SESSION['turma'])) {
     header("Location: ../index.php");
     exit;
@@ -20,63 +21,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $id = $_POST['produto_id'];
 
         // Seleciona o produto na tabela 'pegado'
-        $sql = "SELECT * FROM pegado WHERE id = ?";
+        $sql = "SELECT * FROM pegado WHERE id = ? AND id_turma = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("i", $id);
+        $stmt->bind_param("ii", $id, $turma);
         $stmt->execute();
         $result = $stmt->get_result();
         $produto = $result->fetch_assoc();
 
         if ($produto) {
-            // Verifica se o produto já existe na posição e possui o mesmo nome
-            $sql_check = "SELECT * FROM estoque WHERE posicao = ? AND nome_produto = ?";
+            // Verifica se o produto já existe na posição e possui o mesmo nome e turma
+            $sql_check = "SELECT * FROM estoque WHERE posicao = ? AND nome_produto = ? AND id_turma = ?";
             $stmt_check = $conn->prepare($sql_check);
-            $stmt_check->bind_param("ss", $produto['posicao'], $produto['nome_produto']);
+            $stmt_check->bind_param("ssi", $produto['posicao'], $produto['nome_produto'], $turma);
             $stmt_check->execute();
             $result_check = $stmt_check->get_result();
             $existing_product = $result_check->fetch_assoc();
 
             if ($existing_product) {
-                // Atualiza a quantidade se o produto já existir
+                // Atualiza a quantidade se o produto já existir na mesma turma
                 $nova_quantidade = $existing_product['quantidade_enviada'] + $produto['quantidade_enviada'];
-                $sql_update = "UPDATE estoque SET quantidade_enviada = ? WHERE posicao = ? AND nome_produto = ?";
+                $sql_update = "UPDATE estoque SET quantidade_enviada = ? WHERE posicao = ? AND nome_produto = ? AND id_turma = ?";
                 $stmt_update = $conn->prepare($sql_update);
-                $stmt_update->bind_param("iss", $nova_quantidade, $produto['posicao'], $produto['nome_produto']);
+                $stmt_update->bind_param("issi", $nova_quantidade, $produto['posicao'], $produto['nome_produto'], $turma);
                 $stmt_update->execute();
             } else {
-                // Insere um novo registro no estoque
-                $sql_insert = "INSERT INTO estoque (id_doca, nome_produto, quantidade_enviada, posicao) VALUES (?, ?, ?, ?)";
+                // Insere um novo registro no estoque com a turma
+                $sql_insert = "INSERT INTO estoque (id_doca, nome_produto, quantidade_enviada, posicao, id_turma) VALUES (?, ?, ?, ?, ?)";
                 $stmt_insert = $conn->prepare($sql_insert);
-                $stmt_insert->bind_param("isss", $produto['id_doca'], $produto['nome_produto'], $produto['quantidade_enviada'], $produto['posicao']);
+                $stmt_insert->bind_param("isssi", $produto['id_doca'], $produto['nome_produto'], $produto['quantidade_enviada'], $produto['posicao'], $turma);
                 $stmt_insert->execute();
             }
 
             // Remove o registro da tabela 'pegado'
-            $sql_delete = "DELETE FROM pegado WHERE id = ?";
+            $sql_delete = "DELETE FROM pegado WHERE id = ? AND id_turma = ?";
             $stmt_delete = $conn->prepare($sql_delete);
-            $stmt_delete->bind_param("i", $id);
+            $stmt_delete->bind_param("ii", $id, $turma);
             $stmt_delete->execute();
 
             // Seleciona todos os produtos relacionados ao mesmo id_carga do produto processado
             $id_carga = $produto['id_carga'];
-            $sql_check_carga = "SELECT * FROM pegado WHERE id_carga = ?";
+            $sql_check_carga = "SELECT * FROM pegado WHERE id_carga = ? AND id_turma = ?";
             $stmt_check_carga = $conn->prepare($sql_check_carga);
-            $stmt_check_carga->bind_param("i", $id_carga);
+            $stmt_check_carga->bind_param("ii", $id_carga, $turma);
             $stmt_check_carga->execute();
             $result_check_carga = $stmt_check_carga->get_result();
 
             // Se não houver mais produtos relacionados ao id_carga, remove a entrada da tabela 'docas'
             if ($result_check_carga->num_rows === 0) {
-                $sql_delete_doca = "DELETE FROM docas WHERE id_carga = ?";
+                $sql_delete_doca = "DELETE FROM docas WHERE id_carga = ? AND id_turma = ?";
                 $stmt_delete_doca = $conn->prepare($sql_delete_doca);
-                $stmt_delete_doca->bind_param("i", $id_carga);
+                $stmt_delete_doca->bind_param("ii", $id_carga, $turma);
                 $stmt_delete_doca->execute();
             }
 
             // Atualiza a situação da carga para 'Finalizada'
-            $sql_update_carga = "UPDATE carga SET situacao = 'Finalizada' WHERE id = ?";
+            $sql_update_carga = "UPDATE carga SET situacao = 'Finalizada' WHERE id = ? AND turma_id = ?";
             $stmt_update_carga = $conn->prepare($sql_update_carga);
-            $stmt_update_carga->bind_param("i", $id_carga);
+            $stmt_update_carga->bind_param("ii", $id_carga, $turma);
             $stmt_update_carga->execute();
         }
     }
