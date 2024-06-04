41d1e331c5887c20f733c932efa7576e905a9b7a
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-06-04 08:55:22 -0300

    .

---
+++ telaEstoque.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+if(!isset($_SESSION['id'])){
+    header("Location: index.php");
+    exit;
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_SESSION['turma'] = $_POST['turma'];}
+?>
+<!DOCTYPE html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+<link rel="shortcut icon" href="img/amem.svg">
+
+    <meta charset="utf-8">
+    <title><?php 
+    $tituloPag = 'Estoque';
+    echo "$tituloPag"; 
+    ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/doca.css">
+</head>
+
+<body>
+<?php
+    include 'include/header.php'
+    ?>
+    <main>
+        <?php
+        include 'include/menuLateral.php';
+        
+        ?>
+        <div class="DivDireita">
+            <div class="table-inputs">
+            <div class="text">
+                <h1>Estoque</h1>
+            </div>
+            
+            </div>
+        </div>
+    </main>
+
+
+    <script src="js/ver.js"></script>
+    <script src="js/sidebar.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
