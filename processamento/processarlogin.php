a2a5657d4638e0ff671b5c70b5f3066e88635fbe
lorez <125885188+Fuscaturbinado@users.noreply.github.com>
2024-06-11 18:27:24 -0300

    Update processarlogin.php

--- processamento/processarlogin.php
+++ processamento/processarlogin.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $hostname = "127.0.0.1";
+    $user = "root.Att";
+    $password = "root";
+    $database = "logistica";
+
+    $conexao = new mysqli($hostname, $user, $password, $database);
 
 
 
