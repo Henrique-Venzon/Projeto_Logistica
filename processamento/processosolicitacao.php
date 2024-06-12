fee1845141599ef034067b9dbf9c172a6d167d6c
lorez <125885188+Fuscaturbinado@users.noreply.github.com>
2024-06-12 11:56:09 -0300

    parte solicitacao

--- processamento/processosolicitacao.php
+++ processamento/processosolicitacao.php
@@ -18,25 +18,24 @@ if ($conexao->connect_error) {
 
 $id_pedido = $_POST['npedido'];
 $produto = $_POST['produto'];
-$quantidade = $_POST['quantidade'];
+$quantidade = $_POST['quantidade'] ;
+$produto2 = $_POST['produto2']  ? $_POST['produto2'] : 0 ;
+$quantidade2 = $_POST['quantidade2']  ? $_POST['quantidade2'] : 0 ;
+$produto3 = $_POST['produto3']  ? $_POST['produto3'] : 0 ;
+$quantidade3 = $_POST['quantidade3']  ? $_POST['quantidade3'] : 0 ;
+$produto4 = $_POST['produto4'] ? $_POST['produto4'] : 0 ;
+$quantidade4 = $_POST['quantidade4']  ? $_POST['quantidade4'] : 0 ;
 $sql = "SELECT * FROM `estoque` WHERE nome_produto = '$produto'";
 
 $resultado = $conexao->query($sql);
 
 if ($resultado->num_rows > 0) {
-    // O produto existe no estoque
-    while ($row = $resultado->fetch_assoc()) {
-        if ($row['quantidade_enviada'] >= $quantidade) {
-            // Cria um novo pedido
-            $sql_pedido = "INSERT INTO `pedidos` (id_pedido, nome_produto, quantidade) VALUES ('$id_pedido', '$produto', '$quantidade')";
+            $sql_pedido = "INSERT INTO `pedidos` (id_pedido, produto, quantidade, produto2, quantidade2, produto3, quantidade3, produto4, quantidade4) VALUES ('".$id_pedido."', '".$produto."', '".$quantidade."','".$produto2."', '".$quantidade2."','".$produto3."', '".$quantidade3."','".$produto4."', '".$quantidade4."')";
             if ($conexao->query($sql_pedido) === TRUE) {
                 echo "Pedido criado com sucesso para o produto: $produto ";
+                header ('location:../solicitacao.php');
             } else {
                 echo "Erro ao criar o pedido: " . $conexao->error;
             }
-        } else {
-            echo "Produto não encontrado no estoque.";
-        }
-    }
-}
+        } 
 $conexao->close();
