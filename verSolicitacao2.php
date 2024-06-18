de894e0f568ad08d36dc38470b1895d15d5d765e
Matheus Yan <reismatheusyan83@gmail.com>
2024-06-18 10:33:49 -0300

    Update verSolicitacao2.php

--- verSolicitacao2.php
+++ verSolicitacao2.php
@@ -1,143 +1,138 @@
 <?php
+$id_aluno = 1;
 session_start();
 if (!isset($_SESSION['id'])) {
     header("Location: index.php");
     exit;
 }
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id_pedido = $_POST['id_pedido'];
-}
 if (!isset($_SESSION['turma'])) {
     header("Location: index.php");
     exit;
 }
+$turma = $_SESSION['turma'];
 ?>
 <!DOCTYPE html>
+<html>
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/amem.svg">
-
     <meta charset="utf-8">
-    <title><?php
-    $tituloPag = 'Ver Solicitação';
-    echo "$tituloPag";
-    ?></title>
+    <title><?php $tituloPag = 'Movimentação';
+    echo "$tituloPag"; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet">
-
-    <link rel="stylesheet" href="css/nPedido.css">
+    <link rel="stylesheet" href="css/movDocas.css">
 </head>
 
 <body>
-    <?php
-    $servername = "localhost";
-    $username = "root.Att";
-    $password = "root";
-    $dbname = "logistica";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    $sql = "SELECT `id_pedido`, `produto`, `produto2`, `produto3`, `produto4`,`quantidade`, `quantidade2`, `quantidade3`, `quantidade4`,`id_turma` FROM solicitacao where id_turma = '" . $_SESSION['turma'] . "' and `id_pedido`='" . $id_pedido . "'  ";
-
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $id_pedido = $row['id_pedido'];
-            $produto1 = $row['produto'];
-            $produto2 = $row['produto2'];
-            $produto3 = $row['produto3'];
-            $produto4 = $row['produto4'];
-            $quantidade1 = $row['quantidade'];
-            $quantidade2 = $row['quantidade2'];
-            $quantidade3 = $row['quantidade3'];
-            $quantidade4 = $row['quantidade4'];
-        }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
-
-    ?>
-    <?php
-    include 'include/header.php'
-        ?>
+    <?php include 'include/header.php'; ?>
     <main>
-        <?php
-        include 'include/menuLateral.php';
-
-        ?>
+        <?php include 'include/menuLateral.php'; ?>
         <div class="DivDireita">
             <div class="table-inputs">
-                <div class="txt">
-                    <h1>Pedido</h1>
+                <div class="txtCont">
+                    <h1>Doca 1</h1>
                 </div>
-                <div class="tabela-scroll">
-                    <Div class="nomet">
-                        <h1>Produtos Solicitados:</h1>
-                    </Div>
-                    <table class="table">
-                        <tr>
-                            <th>Produto</th>
-                            <?php
-                            if ($produto2 != '')
-                                print "
-                        <th>Produto 2</th>";
-                            if ($produto3 != '')
-                                print "
-                        <th>Produto 3</th>";
-                            if ($produto4 != '')
-                                print "
-                        <th>Produto 4</th>"; ?>
+                <div class="flex">
+                    <div class="divpegar">
+                    <form method='post' action="processamento/selecionar_carga.php">
+                        <h1 class="pegar">Pegar</h1>
+                        <?php
+                        $servername = "localhost";
+                        $username = "root.Att";
+                        $password = "root";
+                        $dbname = "logistica";
 
-                        </tr>
-                        <tr>
-                            <td><?php echo $produto1; ?></td>
-                            <?php if ($produto2 != '')
-                                echo '<td>' . $produto2 . '</td>'; ?>
-                            <?php if ($produto3 != '')
-                                echo '<td>' . $produto3 . '</td>'; ?>
-                            <?php if ($produto4 != '')
-                                echo '<td>' . $produto4 . '</td>'; ?>
-                        </tr>
+                        $conn = new mysqli($servername, $username, $password, $dbname);
 
-                        <tr>
-                            <th>Quantidade </th>
-                            <?php
-                            if ($quantidade2 != '0')
-                                print "
-                        <th>quantidade 2</th>";
-                            if ($quantidade3 != '0')
-                                print "
-                        <th>qantidade 3</th>";
-                            if ($quantidade4 != '0')
-                                print "
-                        <th>quantidade 4</th>"; ?>
-                        </tr>
-                        <tr>
-                            <td><?php echo $quantidade1; ?></td>
-                            <?php if ($quantidade2 != '0')
-                                echo '<td>' . $quantidade2 . '</td>'; ?>
-                            <?php if ($quantidade3 != '0')
-                                echo '<td>' . $quantidade3 . '</td>'; ?>
-                            <?php if ($quantidade4 != '0')
-                                echo '<td>' . $quantidade4 . '</td>'; ?>
-                        </tr>
+                        $sql = "SELECT * FROM `solicitacao` where id_turma='1' ORDER BY `solicitacao`.`id` ASC";
+                        $res = $conn->query($sql);
+                        $qtd = $res->num_rows;
+
+                        if ($qtd > 0) {
+                            print "<div class=\"tabela-scroll\">";
+                            print "<table class='table' >";
+                            print "<tr>";
+                            print "<th>Produto</th>";
+                            print "<th>Quantidade</th>";
+                            print "<th>Posição</th>";
+                            print "<th>Pegar</th>";
+                            print "</tr>";
+
+                            while ($row = $res->fetch_object()) {
+                                echo "<tr>";
+                                echo "<td style=\"border-right:1px solid black;\">" . $row->produto . "</td>";
+                                echo "<td style=\"border-right:1px solid black;\">" . $row->quantidade . "</td>";
+                                echo "<td style=\"border-right:1px solid black;\">" . $row->posicao . "</td>";
+                                echo "<input class=\"custom-checkbox\" type=\"hidden\" name=\"id_carga\" value=\"" . $row->id_carga . "\">";
+                                echo "<td><input class=\"custom-checkbox\" type=\"checkbox\" name=\"produtos_selecionados[]\" value=\"" . $row->id . "\"></td>";
+                                echo"<input type='hidden' name='doca' value='D1'>";
+                                echo"<input type='hidden' name='doca_id_real' value='1'>";
+                                echo "</tr>";
+                            }
+                            print "</table>";
+                            print "</div>";
+                            print '                        <DIV class="buttonEnviar">
+                            <button>Enviar</button>
+                        </DIV>';
+                        } else {
+                            print "<p class='alert alert-danger'>Não encontrou nenhum pedido nas docas.</p>";
+                        }
+                        ?>
+                    </form>
+                    </div>
+                    <div class="divpegar">
+                    <h1 class="pegar">Finalizar</h1>
+                    <?php
+                    $sql = "SELECT * FROM pegado where id_doca=1";
+                    $res = $conn->query($sql);
+                    $qtd = $res->num_rows;
+
+                    if ($qtd > 0) {
+                        print "<div class=\"tabela-scroll\">";
+                        print "<table class='table' >";
+                        print "<tr>";
+                        print "<th>Produto</th>";
+                        print "<th>Quantidade</th>";
+                        print "<th>Posição</th>";
+                        print "<th>Finalizar</th>";
+                        print "</tr>";
+
+                        while ($row = $res->fetch_object()) {
+                            print "<tr>";
+                            print "<td style=\"border-right:1px solid black;\">" . $row->nome_produto . "</td>";
+                            print "<td style=\"border-right:1px solid black;\">" . $row->quantidade_enviada . "</td>";
+                            print "<td style=\"border-right:1px solid black;\">" . $row->posicao . "</td>";
+                            print "<td>
+                                    <form method='post' action='processamento/processar_movimentacaoD1.php' style='display:inline-block'>
+                                        <input type='hidden' name='produto_id' value='" . $row->id . "'>
+                                        <button class=\"finalizar\" type='submit' name='finalizar'>Finalizar</button>
+                                    </form>
+                                  </td>";
+                            print "</tr>";
+                        }
+                        print "</table>";
+                        print "</div>";
+                    } else {
+                        print"<div class=\"paragrafo\">";
+                        print "<p class='alert alert-danger'>Nenhum produto selecionado.</p>";
+                        print"</div>";
+                    }
+                    ?>
+                </div>
                 </div>
             </div>
         </div>
     </main>
 
-
-    <script src="js/ver.js"></script>
+    <script src="js/movimentar.js"></script>
     <script src="js/sidebar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
