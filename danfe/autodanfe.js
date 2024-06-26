c6b664a25c3cb0a213c3af067a38efe2158bb73b
lorez <125885188+Fuscaturbinado@users.noreply.github.com>
2024-06-26 11:28:16 -0300

    Update autodanfe.js

--- danfe/autodanfe.js
+++ danfe/autodanfe.js
@@ -736,21 +736,26 @@ document.getElementById("preset3").addEventListener("click", function () {
     1000,
     2
   );
-  var totalramdom = parseFloat(document.getElementById("total_produtos").value);
+  
   document.getElementById("valor_frete").value = getRandomDecimal(10, 100, 2);
-  var ramdomFrete = parseFloat(document.getElementById("valor_frete").value);
+ 
   document.getElementById("valor_seguro").value = getRandomDecimal(10, 100, 2);
-  var ramdomSeguro = parseFloat(document.getElementById("valor_seguro").value);
+  
   document.getElementById("desconto").value = getRandomDecimal(10, 100, 2);
   document.getElementById("valor_ipi").value = getRandomDecimal(10, 100, 2);
-  var valorIpi = parseFloat(document.getElementById("valor_ipi").value);
+  
   //var ramdomsDespesas = parseFloat(document.getElementById("outras_despesas").value);
   
   function ramdom (){
     Math.random()
-   var valorramdon = totalramdom + ramdomFrete + ramdomSeguro +  valorIpi;
+    var totalramdom = parseFloat(document.getElementById("total_produtos").value);
+    var ramdomFrete = parseFloat(document.getElementById("valor_frete").value);
+    var ramdomSeguro = parseFloat(document.getElementById("valor_seguro").value);
+    var valorIpi = parseFloat(document.getElementById("valor_ipi").value);
+    var valorramdon = totalramdom + ramdomFrete + ramdomSeguro +  valorIpi;
+    document.getElementById("valor_total_nota").value = parseFloat(document.getElementById(valorramdom));
    }
-  document.getElementById("valor_total_nota").value = document.getElementById(valorramdom);
+  
   document.getElementById("codigo_antt").value = getRandomString(10);
   document.getElementById("placa_veiculo").value =
     getRandomString(3).toUpperCase() + "-" + getRandomNumberString(4);
