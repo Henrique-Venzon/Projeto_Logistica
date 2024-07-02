e3f37e8891b75803b703d80ddc68608cee9bb6e0
Matheus Yan <reismatheusyan83@gmail.com>
2024-07-02 11:55:22 -0300

    .

---
+++ include/conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root.Att";
+$password = "root";
+$dbname = "matrei89_logistica";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
