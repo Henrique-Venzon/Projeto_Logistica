cb1a7a8b07557bf5ff8097b22684a5d6b2c779b8
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2024-07-02 11:28:37 -0300

    inventario atualizado
    puxando pelo id

--- inventario.php
+++ inventario.php
@@ -17,10 +17,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $searchTerm = $_POST['search'];
 
     // Preparar e executar a consulta SQL
-    $sql = "SELECT id_doca, nome_produto, quantidade_enviada, posicao FROM estoque WHERE id_doca LIKE ? OR nome_produto LIKE ? OR quantidade_enviada LIKE ? OR posicao LIKE ?";
+    $sql = "SELECT id, id_doca, nome_produto, quantidade_enviada, posicao 
+            FROM estoque 
+            WHERE id LIKE ? OR id_doca LIKE ? OR nome_produto LIKE ? OR quantidade_enviada LIKE ? OR posicao LIKE ?";
     $stmt = $conn->prepare($sql);
     $searchTermWildcard = "%" . $searchTerm . "%";
-    $stmt->bind_param("ssis", $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard);
+    $stmt->bind_param("sisss", $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard);
     $stmt->execute();
     $result = $stmt->get_result();
 
@@ -28,6 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows > 0) {
         echo "<table border='1'>
                 <tr>
+                    <th>ID</th>
                     <th>ID Doca</th>
                     <th>Nome do Produto</th>
                     <th>Quantidade Enviada</th>
@@ -35,6 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </tr>";
         while($row = $result->fetch_assoc()) {
             echo "<tr>
+                    <td>" . $row["id"] . "</td>
                     <td>" . $row["id_doca"] . "</td>
                     <td>" . $row["nome_produto"] . "</td>
                     <td>" . $row["quantidade_enviada"] . "</td>
@@ -58,7 +62,7 @@ $conn->close();
 <body>
     <h2>Pesquisa no Estoque</h2>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <label for="search">Pesquisar:</label>
+        <label for="search">Pesquisar (ID, ID Doca, Nome, Quantidade Enviada, Posição):</label>
         <input type="text" id="search" name="search">
         <input type="submit" value="Pesquisar">
     </form>
