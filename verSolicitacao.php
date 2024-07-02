da5f2a13028b8f5ca60f276979e7f2fbdb16fbbe
Matheus Yan <reismatheusyan83@gmail.com>
2024-07-02 11:55:15 -0300

    .

--- verSolicitacao.php
+++ verSolicitacao.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     die("Connection failed: " . $conn->connect_error);
                 }
 
-                $sql_after = "SELECT * FROM solicitacao WHERE id_turma = '" . $_SESSION['turma'] . "' ORDER BY id_pedido ASC";
+                $sql_after = "SELECT id FROM solicitacao WHERE id_turma = '" . $_SESSION['turma'] . "' ORDER BY id ASC";
                 $res = $conn->query($sql_after);
                 $qtd = $res->num_rows;
 
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     print "</tr>";
 
                     while ($row = $res->fetch_object()) {
-                        $id_pedido = $row->id_pedido;
+                        $id_pedido = $row->id;
                         $id = $row->id;
                         echo "<tr>";
                         echo "<td>" . $id_pedido . "</td>";
