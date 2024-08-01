59ae69787c60e20ed68d18096769309a815a5100
Matheus Yan <reismatheusyan83@gmail.com>
2024-08-01 19:14:14 -0300

    Update inventario.php

--- inventario.php
+++ inventario.php
@@ -1,7 +1,7 @@
 <!--Corpo -->
 <?php
 session_start();
-if(!isset($_SESSION['id'])){
+if (!isset($_SESSION['id'])) {
     header("Location: index.php");
     exit;
 }
@@ -10,73 +10,18 @@ if (($_SESSION['tipo_login'] != 'professor')) {
     exit;
 }
 ?>
-<?php
-$servername = "localhost";
-$username = "root.Att";
-$password = "root";
-$dbname = "logistica";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Verificar conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-}
-
-// Verificar se o formulário foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $searchTerm = $_POST['search'];
-
-    // Preparar e executar a consulta SQL
-    $sql = "SELECT id, id_doca, nome_produto, quantidade_enviada, posicao 
-            FROM estoque 
-            WHERE id LIKE ? OR id_doca LIKE ? OR nome_produto LIKE ? OR quantidade_enviada LIKE ? OR posicao LIKE ?";
-    $stmt = $conn->prepare($sql);
-    $searchTermWildcard = "%" . $searchTerm . "%";
-    $stmt->bind_param("sisss", $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    // Exibir resultados
-    if ($result->num_rows > 0) {
-        echo "<table border='1'>
-                <tr>
-                    <th>ID</th>
-                    <th>ID Doca</th>
-                    <th>Nome do Produto</th>
-                    <th>Quantidade Enviada</th>
-                    <th>Posição</th>
-                </tr>";
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>" . $row["id"] . "</td>
-                    <td>" . $row["id_doca"] . "</td>
-                    <td>" . $row["nome_produto"] . "</td>
-                    <td>" . $row["quantidade_enviada"] . "</td>
-                    <td>" . $row["posicao"] . "</td>
-                  </tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "Nenhum resultado encontrado.";
-    }
-    $stmt->close();
-}
-$conn->close();
-
-
-?>
 <!DOCTYPE html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php
-    $tituloPag = 'Home';
+    $tituloPag = 'inventario';
     echo "$tituloPag";
     ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <link rel="shortcut icon" href="img/amem.svg">
+    <link rel="shortcut icon" href="img/amem.svg">
 
     <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet">
@@ -95,12 +40,52 @@ $conn->close();
         include 'include/menuLateral.php'
             ?>
 
-       </main<h2>Pesquisa no Estoque</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <label for="search">Pesquisar (ID, ID Doca, Nome, Quantidade Enviada, Posição):</label>
-    <input type="text" id="search" name="search">
-    <input type="submit" value="Pesquisar">
+        </main<h2>Pesquisa no Estoque</h2>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label for="search">Pesquisar (ID, ID Doca, Nome, Quantidade Enviada, Posição):</label>
+            <input type="text" id="search" name="search">
+            <input type="submit" value="Pesquisar">
+            <?php
+            include_once ('include/conexao.php');
 
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $searchTerm = $_POST['search'];
+
+                $sql = "SELECT id, nome_produto, quantidade_enviada, posicao
+            FROM estoque 
+            WHERE id LIKE ? OR nome_produto LIKE ? OR quantidade_enviada LIKE ? OR posicao LIKE ?";
+                $stmt = $conn->prepare($sql);
+                $searchTermWildcard = "%" . $searchTerm . "%";
+                $stmt->bind_param("isis", $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows > 0) {
+                    echo "<table border='1'>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome do Produto</th>
+                    <th>Quantidade Enviada</th>
+                    <th>Posição</th>
+                </tr>";
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>
+                    <td>" . $row["id"] . "</td>
+                    <td>" . $row["nome_produto"] . "</td>
+                    <td>" . $row["quantidade_enviada"] . "</td>
+                    <td>" . $row["posicao"] . "</td>
+                  </tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "Nenhum resultado encontrado.";
+                }
+                $stmt->close();
+            }
+            $conn->close();
+
+
+            ?>
 
     </main>
 
