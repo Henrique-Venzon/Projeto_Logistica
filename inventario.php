15ec21d47a05a848c70f4c3abf7c7df320e2ce57
Matheus Yan <reismatheusyan83@gmail.com>
2024-08-14 07:58:25 -0300

    Update inventario.php

--- inventario.php
+++ inventario.php
@@ -1,28 +1,24 @@
-<!--Corpo -->
 <?php
 session_start();
 if (!isset($_SESSION['id'])) {
     header("Location: index.php");
     exit;
 }
-
 ?>
+
 <!DOCTYPE html>
+<html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php
-    $tituloPag = 'inventario';
-    echo "$tituloPag";
-    ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+            $tituloPag = 'inventario';
+            echo "$tituloPag";
+            ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="shortcut icon" href="img/amem.svg">
-
-    <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap"
-        rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/inventario.css">
     <script src="js/reveal.js" defer></script>
 </head>
@@ -30,74 +26,86 @@ if (!isset($_SESSION['id'])) {
 <body>
     <?php
     include 'include/header.php'
-        ?>
+    ?>
     <main>
         <?php
         include 'include/menuLateral.php'
-            ?>
-<div class="DivDireita">
-<div class="table-inputs">
-        <div class="txt">
-            <h1>Pesquisa no Estoque</h1>
-        </div>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            <label for="search">Pesquisar (ID, Nome, Quantidade, Posição):</label>
-            <input type="text" id="search" name="search">
-            <input class="enviar" type="submit" value="Pesquisar">
-            <?php
-            include_once ('include/conexao.php');
+        ?>
+        <div class="DivDireita">
+            <div class="table-inputs">
+                <div class="txt">
+                    <h1>Pesquisa no Estoque</h1>
+                </div>
+                <form method="post" action="">
+                    <label for="search">Pesquisar:</label>
+                    <input type="text" id="search" name="search">
+                    <select name="search_type">
+                        <option value="id">ID</option>
+                        <option value="nome_produto">Nome do Produto</option>
+                        <option value="quantidade_enviada">Quantidade</option>
+                        <option value="posicao">Posição</option>
+                    </select>
+                    <input class="enviar" type="submit" value="Pesquisar">
+                </form>
 
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $searchTerm = $_POST['search'];
+                <?php
+                include_once('include/conexao.php');
 
-                $sql = "SELECT id, nome_produto, quantidade_enviada, posicao
-            FROM estoque 
-            WHERE id LIKE ? OR nome_produto LIKE ? OR quantidade_enviada LIKE ? OR posicao LIKE ?";
-                $stmt = $conn->prepare($sql);
-                $searchTermWildcard = "%" . $searchTerm . "%";
-                $stmt->bind_param("isis", $searchTermWildcard, $searchTermWildcard, $searchTermWildcard, $searchTermWildcard);
-                $stmt->execute();
-                $result = $stmt->get_result();
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $searchTerm = $_POST['search'];
+                    $searchType = $_POST['search_type'];
 
-                if ($result->num_rows > 0) {
-                    echo "<table border='1'>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome do Produto</th>
-                    <th>Quantidade Enviada</th>
-                    <th>Posição</th>
-                </tr>";
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>
-                    <td>" . $row["id"] . "</td>
-                    <td>" . $row["nome_produto"] . "</td>
-                    <td>" . $row["quantidade_enviada"] . "</td>
-                    <td>" . $row["posicao"] . "</td>
-                  </tr>";
+                    // Prepara a query SQL com base no tipo de pesquisa
+                    $sql = "SELECT id, nome_produto, quantidade_enviada, posicao FROM estoque WHERE ";
+                    if ($searchType === 'nome_produto') {
+                        $sql .= "nome_produto LIKE ?"; 
+                    } else {
+                        $sql .= "$searchType = ?";
                     }
-                    echo "</table>";
-                } else {
-                    echo "Nenhum resultado encontrado.";
+
+                    $stmt = $conn->prepare($sql);
+                    
+                    if ($searchType === 'nome_produto') {
+                        $searchTerm = "%" . $searchTerm . "%";
+                        $stmt->bind_param("s", $searchTerm); 
+                    } else {
+                        $stmt->bind_param("s", $searchTerm); 
+                    }
+                    
+
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+
+                    if ($result->num_rows > 0) {
+                        echo "<table border='1'>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome do Produto</th>
+                            <th>Quantidade</th>
+                            <th>Posição</th>
+                        </tr>";
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>
+                            <td>" . $row["id"] . "</td>
+                            <td>" . $row["nome_produto"] . "</td>
+                            <td>" . $row["quantidade_enviada"] . "</td>
+                            <td>" . $row["posicao"] . "</td>
+                        </tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "Nenhum resultado encontrado.";
+                    }
+                    $stmt->close();
                 }
-                $stmt->close();
-            }
-            $conn->close();
-
-
-            ?>
-</form>
-</div>
-</div>
-
-
+                $conn->close();
+                ?>
+            </div>
+        </div>
     </main>
 
-
-
     <script src="js/sidebar.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
